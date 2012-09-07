18adfd554a7c9be6486ef9e3665b1095d0096a98
Julian Hochstetter <julian.hochstetter@mni.thm.de>
2012-09-07 09:48:21 +0200

    add welcome controller which redirects simply to index.html

---
+++ src/main/java/de/thm/arsnova/WelcomeController.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2012 THM webMedia
+ * 
+ * This file is part of ARSnova.
+ *
+ * ARSnova is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ARSnova is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.thm.arsnova;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class WelcomeController {
+	@RequestMapping(method = RequestMethod.GET, value = "/")
+	public ModelAndView home(HttpServletRequest request) {
+		String referer = request.getHeader("referer");
+		String target = "index.html";
+		if (referer != null && referer.endsWith("dojo-index.html")) {
+			target = "dojo-index.html";
+		}
+		return new ModelAndView("redirect:/" + target);
+	}
+}
