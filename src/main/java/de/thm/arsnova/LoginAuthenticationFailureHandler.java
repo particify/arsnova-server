dab51de451622ac2394f2f4ada4c3ef35c06979e
Julian Hochstetter <julian.hochstetter@mni.thm.de>
2012-09-12 20:47:17 +0200

    Add AuthenticationFailureHandler which redirects to ARSnova Home, using in session stored referer

---
+++ src/main/java/de/thm/arsnova/LoginAuthenticationFailureHandler.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2012 THM webMedia
+ * 
+ * This file is part of ARSnova.
+ *
+ * ARSnova is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ARSnova is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.thm.arsnova;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+public class LoginAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+	
+	private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+	private String defaultFailureUrl;
+	
+	@Override
+	public void onAuthenticationFailure(HttpServletRequest request,
+			HttpServletResponse response, AuthenticationException exception)
+			throws IOException, ServletException {
+		
+		HttpSession session = request.getSession();
+		if (session != null && session.getAttribute("ars-referer") != null) {
+			defaultFailureUrl = (String) session.getAttribute("ars-referer");
+		}
+		
+		redirectStrategy.sendRedirect(request, response, defaultFailureUrl);
+	}
+	
+	public void setDefaultFailureUrl(String defaultFailureUrl) {
+		this.defaultFailureUrl = defaultFailureUrl;
+	}
+
+}
