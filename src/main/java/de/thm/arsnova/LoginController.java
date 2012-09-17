d74b6f5707af45dddbf1c2e8491ce1e0eb90ecd2
Julian Hochstetter <julian.hochstetter@mni.thm.de>
2012-09-17 20:45:09 +0200

    add @ResponseBody Annotation to whoami action

--- src/main/java/de/thm/arsnova/LoginController.java
+++ src/main/java/de/thm/arsnova/LoginController.java
@@ -46,6 +46,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.view.RedirectView;
@@ -110,6 +111,7 @@ public class LoginController {
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "/whoami")
+	@ResponseBody
 	public User whoami() {
 		return userService.getUser(SecurityContextHolder.getContext().getAuthentication());
 	}
