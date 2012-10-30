1f140667787fc219b7eec000cf5161508776317e
Christoph Thelen <christoph.thelen@mni.thm.de>
2012-10-30 14:01:11 +0100

    Use custom tomcat port. Minor code layout changes.

--- src/test/java/de/thm/arsnova/Selenium2Test.java
+++ src/test/java/de/thm/arsnova/Selenium2Test.java
@@ -20,7 +20,7 @@ public class Selenium2Test {
 
 	ARSnovaChromeDriver driver;
 	Properties properties;
-	
+
 	CouchDBDao couchdbDao;
 	SessionService sessionService;
 	StubUserService userService;
@@ -29,10 +29,10 @@ public class Selenium2Test {
 	public void setUp() throws IOException {
 		Resource resource = new FileSystemResource("/etc/arsnova/arsnova.properties");
 		properties = PropertiesLoaderUtils.loadProperties(resource);
-		
+
 		userService = new StubUserService();
 		userService.setUserAuthenticated(true);
-		
+
 		couchdbDao = new CouchDBDao();
 		couchdbDao.setDatabaseHost(properties.getProperty("couchdb.host", "localhost"));
 		couchdbDao.setDatabasePort(properties.getProperty("couchdb.port", "5984"));
@@ -41,7 +41,7 @@ public class Selenium2Test {
 		couchdbDao.setSessionService(sessionService);
 		couchdbDao.setUserService(userService);
 		sessionService.setDatabaseDao(couchdbDao);
-		
+
 		this.driver = new ARSnovaChromeDriver();
 		driver.get(properties.getProperty("security.arsnova-url", "http://localhost:8080/arsnova-war/"));
 		LocalStorage localStorage = this.driver.getLocalStorage();
@@ -53,32 +53,32 @@ public class Selenium2Test {
 		driver.close();
 		driver.quit();
 	}
-	
+
 	@Test
 	public void studentGuestShouldBeAbleToJoinSession() {
 		Session session = new Session();
 		session.setName("selenium test session");
 		session.setShortName("selenium");
 		session = couchdbDao.saveSession(session);
-		
+
 		WebElement studentRoleButton = waitForElement(By.id("ext-gen1047"));
 		studentRoleButton.click();
-		
+
 		WebElement guestLoginButton = waitForElement(By.id("ext-gen1016"));
 		guestLoginButton.click();
-		
+
 		WebElement sessionKeywordField = waitForElement(By.name("keyword"));
 		sessionKeywordField.sendKeys(session.getKeyword());
 		WebElement joinSessionButton = waitForElement(By.id("ext-gen1138"));
 		joinSessionButton.click();
-		
+
 		WebElement feedbackGoodButton = waitForElement(By.className("feedbackGood"));
 		assertTrue(feedbackGoodButton.isDisplayed());
 	}
-	
+
 	private WebElement waitForElement(final By by) {
 		(new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
-			public Boolean apply(WebDriver d) {
+			public Boolean apply(final WebDriver d) {
 				return d.findElement(by) != null;
 			}
 		});
