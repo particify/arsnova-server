4b17a3388cee441c5891a93c925f5a9cbaed54d2
Christoph Thelen <christoph.thelen@mni.thm.de>
2012-10-31 19:36:53 +0100

    Test posting of feedback for student guests

--- src/main/java/de/thm/arsnova/entities/Feedback.java
+++ src/main/java/de/thm/arsnova/entities/Feedback.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2012 THM webMedia
+ *
+ * This file is part of ARSnova.
+ *
+ * ARSnova is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ARSnova is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.thm.arsnova.entities;
 
 import java.util.ArrayList;
@@ -24,5 +42,41 @@ public class Feedback {
 
 	public final List<Integer> getValues() {
 		return values;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Feedback)) {
+			return false;
+		}
+		Feedback other = (Feedback) obj;
+
+		if (this.values.size() != other.values.size()) {
+			return false;
+		}
+
+		boolean result = true;
+		for (int i = 0; i < values.size(); i++) {
+			result = result && (this.values.get(i).equals(other.values.get(i)));
+		}
+		return result;
+	}
+
+	@Override
+	public int hashCode() {
+		// See http://stackoverflow.com/a/113600
+		int result = 42;
+		for (Integer v : values) {
+			result = 37 * result + v;
+		}
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return "Feedback [values=" + values + "]";
 	}
 }
--- src/test/java/de/thm/arsnova/Selenium2Test.java
+++ src/test/java/de/thm/arsnova/Selenium2Test.java
@@ -13,13 +13,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.html5.LocalStorage;
 import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PropertiesLoaderUtils;
 
 import de.thm.arsnova.dao.CouchDBDao;
+import de.thm.arsnova.entities.Feedback;
 import de.thm.arsnova.entities.Session;
+import de.thm.arsnova.services.FeedbackService;
 import de.thm.arsnova.services.SessionService;
 import de.thm.arsnova.services.StubUserService;
 
@@ -31,6 +34,7 @@ public class Selenium2Test {
 	private CouchDBDao couchdbDao;
 	private SessionService sessionService;
 	private StubUserService userService;
+	private FeedbackService feedbackService;
 
 	@Before
 	public final void setUp() throws IOException {
@@ -48,6 +52,8 @@ public class Selenium2Test {
 		couchdbDao.setSessionService(sessionService);
 		couchdbDao.setUserService(userService);
 		sessionService.setDatabaseDao(couchdbDao);
+		feedbackService = new FeedbackService();
+		feedbackService.setDatabaseDao(couchdbDao);
 
 		this.driver = new ARSnovaChromeDriver();
 		driver.get(properties.getProperty("security.arsnova-url", "http://localhost:8080/arsnova-war/"));
@@ -62,22 +68,41 @@ public class Selenium2Test {
 	}
 
 	@Test
-	public final void studentGuestShouldSeeFeedbackButtonsAfterJoiningSession() {
+	public final void studentGuestShouldPostFeedback() {
 		Session session = couchdbDao.saveSession(createSession());
+		Feedback initialFeedback = feedbackService.getFeedback(session.getKeyword());
 
-		WebElement studentRoleButton = waitForElement(By.id("ext-gen1047"));
-		studentRoleButton.click();
+		selectStudentRole();
+		loginAsGuest();
+		joinSession(session);
 
-		WebElement guestLoginButton = waitForElement(By.id("ext-gen1016"));
-		guestLoginButton.click();
+		WebElement feedbackBadButton = waitForElement(By.className("feedbackBad"));
+		// Before clicking, ensure that no loading mask is displayed
+		By loadingSpinner = By.className("x-mask");
+		waitWhileVisible(loadingSpinner);
+		feedbackBadButton.click();
+		
+		// Wait for the feedback to arrive back at the client
+		By feedbackResultToolbar = By.id("ext-comp-1125");
+		waitForElementWithContent(feedbackResultToolbar, "1/");
+		
+		Feedback feedback = feedbackService.getFeedback(session.getKeyword());
+		assertEquals(new Feedback(0, 0, 0, 0), initialFeedback);
+		assertEquals(new Feedback(0, 0, 1, 0), feedback);
+	}
 
-		WebElement sessionKeywordField = waitForElement(By.name("keyword"));
-		sessionKeywordField.sendKeys(session.getKeyword());
-		WebElement joinSessionButton = waitForElement(By.id("ext-gen1138"));
-		joinSessionButton.click();
+	private void waitForElementWithContent(final By by, final String content) {
+		final long timeoutInSecs = 10;
+		(new WebDriverWait(driver, timeoutInSecs)).until(new ExpectedCondition<Boolean>() {
+			public Boolean apply(final WebDriver d) {
+				WebElement element = d.findElement(by);
+				return element != null && element.getText().contains(content);
+			}
+		});
+	}
 
-		WebElement feedbackGoodButton = waitForElement(By.className("feedbackGood"));
-		assertTrue(feedbackGoodButton.isDisplayed());
+	private void assertEquals(Feedback feedback, Feedback feedback2) {
+		assertTrue(feedback.equals(feedback2));
 	}
 
 	private WebElement waitForElement(final By by) {
@@ -89,6 +114,11 @@ public class Selenium2Test {
 		});
 		return driver.findElement(by);
 	}
+	
+	private void waitWhileVisible(final By by) {
+		final long timeoutInSecs = 10;
+		(new WebDriverWait(driver, timeoutInSecs)).until(ExpectedConditions.invisibilityOfElementLocated(by));
+	}
 
 	private Session createSession() {
 		return createNamedSession(null, null);
@@ -99,5 +129,22 @@ public class Selenium2Test {
 		session.setName(name != null ? name : "selenium test session");
 		session.setShortName(shortName != null ? shortName : "selenium");
 		return session;
+	}
+
+	private void joinSession(Session session) {
+		WebElement sessionKeywordField = waitForElement(By.name("keyword"));
+		sessionKeywordField.sendKeys(session.getKeyword());
+		WebElement joinSessionButton = waitForElement(By.id("ext-gen1138"));
+		joinSessionButton.click();
+	}
+
+	private void loginAsGuest() {
+		WebElement guestLoginButton = waitForElement(By.id("ext-gen1016"));
+		guestLoginButton.click();
+	}
+
+	private void selectStudentRole() {
+		WebElement studentRoleButton = waitForElement(By.id("ext-gen1047"));
+		studentRoleButton.click();
 	}
 }
