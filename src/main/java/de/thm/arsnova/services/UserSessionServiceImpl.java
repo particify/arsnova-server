4b5ae3fd143f5c59762fe41cb3571581115cc2a5
Paul-Christian Volkmer <paul-christian.volkmer@mni.thm.de>
2013-06-04 18:24:15 +0200

    Removed session scope

--- src/main/java/de/thm/arsnova/services/UserSessionServiceImpl.java
+++ src/main/java/de/thm/arsnova/services/UserSessionServiceImpl.java
@@ -1,13 +1,11 @@
 package de.thm.arsnova.services;
 
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import de.thm.arsnova.entities.Session;
 import de.thm.arsnova.entities.User;
 
 @Component
-@Scope("session")
 public class UserSessionServiceImpl implements UserSessionService {
 	
 	private User user;
