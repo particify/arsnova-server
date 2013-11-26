322302fd2823d54074ad19cedaf8b307a3abf688
Daniel Gerhardt <daniel.gerhardt@mni.thm.de>
2013-11-26 15:10:13 +0100

    Fix deletion of free text answers (refs #7880)
    There was a bug in the session retrieval code causing an exception.

--- src/main/java/de/thm/arsnova/services/QuestionService.java
+++ src/main/java/de/thm/arsnova/services/QuestionService.java
@@ -386,7 +386,7 @@ public class QuestionService implements IQuestionService {
 			throw new NotFoundException();
 		}
 		User user = userService.getCurrentUser();
-		Session session = this.databaseDao.getSessionFromId(question.getSessionId());
+		Session session = this.databaseDao.getSessionFromKeyword(question.getSessionKeyword());
 		if (user == null || session == null || !session.isCreator(user)) {
 			throw new UnauthorizedException();
 		}
