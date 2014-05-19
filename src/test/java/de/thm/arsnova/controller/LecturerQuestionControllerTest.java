f29a551445e1ff96f929badc19aac2d42d5d09e8
Paul-Christian Volkmer <paul-christian.volkmer@mni.thm.de>
2014-05-19 11:01:32 +0200

    Removed unused tests

--- src/test/java/de/thm/arsnova/controller/LecturerQuestionControllerTest.java
+++ src/test/java/de/thm/arsnova/controller/LecturerQuestionControllerTest.java
@@ -1,14 +1,9 @@
 package de.thm.arsnova.controller;
 
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-
 import javax.inject.Inject;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -17,19 +12,16 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.servlet.HandlerAdapter;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.annotation.AnnotationMethodHandlerAdapter;
 
 import de.thm.arsnova.dao.StubDatabaseDao;
-import de.thm.arsnova.exceptions.NotFoundException;
-import de.thm.arsnova.exceptions.UnauthorizedException;
 import de.thm.arsnova.services.StubUserService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
 		"file:src/main/webapp/WEB-INF/spring/arsnova-servlet.xml",
 		"file:src/main/webapp/WEB-INF/spring/spring-main.xml",
-		"file:src/test/resources/test-config.xml" })
+"file:src/test/resources/test-config.xml" })
 public class LecturerQuestionControllerTest {
 
 	@Inject
@@ -40,10 +32,10 @@ public class LecturerQuestionControllerTest {
 
 	@Autowired
 	private LecturerQuestionController lecturerQuestionController;
-	
+
 	@Autowired
 	private StubUserService userService;
-	
+
 	@Autowired
 	private StubDatabaseDao databaseDao;
 
@@ -59,33 +51,4 @@ public class LecturerQuestionControllerTest {
 		handlerAdapter = applicationContext
 				.getBean(AnnotationMethodHandlerAdapter.class);
 	}
-
-	/* TODO: update test case for API changes or remove it if it is not necessary anymore */
-	@Ignore
-	@Test(expected=NotFoundException.class)
-	public void testShouldNotGetQestionIdsForUnknownSession() throws Exception {
-		userService.setUserAuthenticated(true);
-		
-		request.setMethod("GET");
-		request.setRequestURI("/session/00000000/questionids");
-		final ModelAndView mav = handlerAdapter.handle(request, response, lecturerQuestionController);
-		
-		assertNull(mav);
-		assertTrue(response.getStatus() == 404);
-	}
-	
-	/* TODO: update test case for API changes or remove it if it is not necessary anymore */
-	@Ignore
-	@Test(expected=UnauthorizedException.class)
-	public void testShouldNotGetQuestionIdsIfUnauthorized() throws Exception {
-		userService.setUserAuthenticated(false);
-		
-		request.setMethod("GET");
-		request.setRequestURI("/session/00000000/questionids");
-		final ModelAndView mav = handlerAdapter.handle(request, response, lecturerQuestionController);
-		
-		assertNull(mav);
-		assertTrue(response.getStatus() == 401);
-	}
-
 }
