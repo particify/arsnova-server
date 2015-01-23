87694604f61c6c7e00dfbe893ed60d9ec27ef1e6
agrt56 <andreas.gaertner@mni.thm.de>
2015-01-23 22:32:14 +0100

    Added getAbstentionAnswerCount to StubDatabaseDao.java

--- src/test/java/de/thm/arsnova/dao/StubDatabaseDao.java
+++ src/test/java/de/thm/arsnova/dao/StubDatabaseDao.java
@@ -555,4 +555,10 @@ public class StubDatabaseDao implements IDatabaseDao {
 		// TODO Auto-generated method stub
 
 	}
+
+	@Override
+	public int getAbstentionAnswerCount(String questionId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
