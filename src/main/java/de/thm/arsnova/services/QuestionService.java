6e73487dd58b9033f02cf263b7a8902aa0df6d13
agrt56 <andreas.gaertner@mni.thm.de>
2015-01-26 14:39:53 +0100

    NewQuestionEvent is triggered when session is set to "active". Task #14448

--- src/main/java/de/thm/arsnova/services/QuestionService.java
+++ src/main/java/de/thm/arsnova/services/QuestionService.java
@@ -410,8 +410,15 @@ public class QuestionService implements IQuestionService, ApplicationEventPublis
 		} else if (question.getPiRound() < 1 || question.getPiRound() > 2) {
 			question.setPiRound(oldQuestion.getPiRound() > 0 ? oldQuestion.getPiRound() : 1);
 		}
+		
+		final Question result = databaseDao.updateQuestion(question);
 
-		return databaseDao.updateQuestion(question);
+		if(!oldQuestion.isActive() && question.isActive()) {
+			final NewQuestionEvent event = new NewQuestionEvent(this, result, session);
+			this.publisher.publishEvent(event);
+		}
+		
+		return result;
 	}
 
 	@Override
