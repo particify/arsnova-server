bf6e45c85b4f3b31cccb809a78956e3a7e2b6915
tekay <tom.kaesler@mni.thm.de>
2015-03-05 08:49:03 +0100

    git says to commit

--- src/main/java/de/thm/arsnova/entities/Session.java
+++ src/main/java/de/thm/arsnova/entities/Session.java
@@ -49,6 +49,9 @@ public class Session implements Serializable {
 	private String ppFaculty;
 	private String ppLevel;
 	private String sessionType;
+    
+    private boolean sortPreparationQuestions;
+    private boolean sortLectureQuestions;
 
 	private String _id;
 	private String _rev;
@@ -288,6 +291,22 @@ public class Session implements Serializable {
 	public void setSessionType(final String sessionType) {
 		this.sessionType = sessionType;
 	}
+    
+    public boolean getSortPreparationQuestions() {
+        return sortIndexPreparation;
+    }
+    
+    public void setSortPreparationQuestions(final boolean SortPreparationQuestions) {
+        this.sortIndexPreparation = sortIndexPreparation;
+    }
+    
+    public boolean getSortPreparationQuestions() {
+        return sortIndexPreparation;
+    }
+    
+    public void setSortLectureQuestions(final boolean SortLectureQuestions) {
+        this.sortIndexLecture = sortIndexLecture;
+    }
 
 	@Override
 	public String toString() {
