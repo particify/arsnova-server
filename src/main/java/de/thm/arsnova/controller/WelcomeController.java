2477ee1f8dbe522686ea1d9df263b976a65b9a4d
agrt56 <andreas.gaertner@mni.thm.de>
2015-05-12 14:42:44 +0200

    Task #15824: Add method to check content for 'x-frame-options' header.

--- src/main/java/de/thm/arsnova/controller/WelcomeController.java
+++ src/main/java/de/thm/arsnova/controller/WelcomeController.java
@@ -22,12 +22,19 @@ import java.util.HashMap;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.view.RedirectView;
+
+import de.thm.arsnova.exceptions.NoContentException;
 
 @Controller
 public class WelcomeController extends AbstractController {
@@ -46,4 +53,17 @@ public class WelcomeController extends AbstractController {
 		return new HashMap<String, Object>();
 	}
 
+	@RequestMapping(value = "/checkFrameOptionsHeader", method = RequestMethod.GET) 
+	@ResponseStatus(HttpStatus.OK)
+	public void checkFrameOptionsHeader(
+			@RequestParam(required = true) final String url
+		) {
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+
+		if (!HttpStatus.OK.equals(response.getStatusCode()) ||
+			response.getHeaders().containsKey("x-frame-options")) {
+				throw new NoContentException();
+		}
+	}
 }
