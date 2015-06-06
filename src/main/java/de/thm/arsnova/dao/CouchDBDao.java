075892f12036e5a7b8c1c6cc81709a1e3387d5a7
Katharina Staden <katharina.staden@mni.thm.de>
2015-06-06 18:22:42 +0200

    Removed @Chacheable from getSession function, because the Update of a session wont work if only the cache is used for loading the content of a session

--- src/main/java/de/thm/arsnova/dao/CouchDBDao.java
+++ src/main/java/de/thm/arsnova/dao/CouchDBDao.java
@@ -432,7 +432,6 @@ public class CouchDBDao implements IDatabaseDao, ApplicationEventPublisherAware 
 	}
 
 	@Override
-	@Cacheable("sessions")
 	public Session getSessionFromKeyword(final String keyword) {
 		final NovaView view = new NovaView("session/by_keyword");
 		view.setKey(keyword);
