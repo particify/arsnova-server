particify/arsnova-server

commit 2ecbfec8b1676781a57f8622908af7cc44abfd48

Migrate remaining Spring XML config to Java

src/main/java/de/thm/arsnova/config/AppConfig.java
@@ -1,0 +1,257 @@
+/*
+ * This file is part of ARSnova Backend.
+ * Copyright (C) 2012-2017 The ARSnova Team
+ *
+ * ARSnova Backend is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ARSnova Backend is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.thm.arsnova.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import de.thm.arsnova.ImageUtils;
+import de.thm.arsnova.connector.client.ConnectorClient;
+import de.thm.arsnova.connector.client.ConnectorClientImpl;
+import de.thm.arsnova.socket.ARSnovaSocket;
+import de.thm.arsnova.socket.ARSnovaSocketIOServer;
+import de.thm.arsnova.web.CacheControlInterceptorHandler;
+import de.thm.arsnova.web.CorsFilter;
+import de.thm.arsnova.web.DeprecatedApiInterceptorHandler;
+import de.thm.arsnova.web.ResponseInterceptorHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Loads property file and configures non-security related beans and components.
+ *
+ * expose-proxy for AspectJ is needed to access the proxy object via AopContext.currentProxy() in CouchDBDao. It might
+ * have a negative impact on performance but is needed for caching until a better solution is implemented (e.g. use of
+ * AspectJ's weaving).
+ */
+@ComponentScan({
+		"de.thm.arsnova.aop",
+		"de.thm.arsnova.cache",
+		"de.thm.arsnova.controller",
+		"de.thm.arsnova.domain",
+		"de.thm.arsnova.dao",
+		"de.thm.arsnova.events",
+		"de.thm.arsnova.security",
+		"de.thm.arsnova.services",
+		"de.thm.arsnova.web"})
+@Configuration
+@EnableAspectJAutoProxy(exposeProxy = true)
+@EnableCaching
+@EnableWebMvc
+@PropertySource(
+		value = {"classpath:arsnova.properties.example", "file:/etc/arsnova/arsnova.properties"},
+		ignoreResourceNotFound = true
+)
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+	@Autowired
+	private Environment env;
+
+	@Value(value = "${connector.enable}") private boolean connectorEnable;
+	@Value(value = "${connector.uri}") private String connectorUri;
+	@Value(value = "${connector.username}") private String connectorUsername;
+	@Value(value = "${connector.password}") private String connectorPassword;
+
+	@Value(value = "${socketio.bind-address}") private String socketAddress;
+	@Value(value = "${socketio.port}") private int socketPort;
+	@Value(value = "${socketio.ssl.jks-file:}") private String socketKeystore;
+	@Value(value = "${socketio.ssl.jks-password:}") private String socketKeystorePassword;
+	@Value(value = "${security.cors.origins:}") private String[] corsOrigins;
+	@Value(value = "${mail.host}") private String mailHost;
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(stringMessageConverter());
+		converters.add(jsonMessageConverter());
+		//converters.add(new MappingJackson2XmlHttpMessageConverter(builder.createXmlMapper(true).build()));
+	}
+
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		configurer.mediaType("json", MediaType.APPLICATION_JSON_UTF8);
+		configurer.mediaType("xml", MediaType.APPLICATION_XML);
+		configurer.mediaType("html", MediaType.TEXT_HTML);
+		configurer.defaultContentType(MediaType.APPLICATION_JSON_UTF8);
+		configurer.favorParameter(true);
+		configurer.favorPathExtension(false);
+	}
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.viewResolver(new InternalResourceViewResolver());
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(cacheControlInterceptorHandler());
+		registry.addInterceptor(deprecatedApiInterceptorHandler());
+		registry.addInterceptor(responseInterceptorHandler());
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger.json").addResourceLocations("classpath:/");
+		registry.addResourceHandler("/**").addResourceLocations("/");
+	}
+
+	@Bean
+	public CacheControlInterceptorHandler cacheControlInterceptorHandler() {
+		return new CacheControlInterceptorHandler();
+	}
+
+	@Bean
+	public DeprecatedApiInterceptorHandler deprecatedApiInterceptorHandler() {
+		return new DeprecatedApiInterceptorHandler();
+	}
+
+	@Bean
+	public ResponseInterceptorHandler responseInterceptorHandler() {
+		return new ResponseInterceptorHandler();
+	}
+
+	@Bean
+	public StringHttpMessageConverter stringMessageConverter() {
+		StringHttpMessageConverter messageConverter = new StringHttpMessageConverter();
+		List<MediaType> mediaTypes = new ArrayList<>();
+		mediaTypes.add(MediaType.TEXT_PLAIN);
+		messageConverter.setSupportedMediaTypes(mediaTypes);
+
+		return messageConverter;
+	}
+
+	@Bean
+	public MappingJackson2HttpMessageConverter jsonMessageConverter() {
+		Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+		builder.serializationInclusion(JsonInclude.Include.NON_NULL);
+		builder.serializationInclusion(JsonInclude.Include.NON_EMPTY);
+		builder.defaultViewInclusion(false);
+		builder.indentOutput(true).simpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+
+		return new MappingJackson2HttpMessageConverter(builder.build());
+	}
+
+//	@Bean
+//	public PropertyPlaceholderConfigurer propertyPlaceholderConfigurer() {
+//		final PropertyPlaceholderConfigurer configurer = new PropertyPlaceholderConfigurer();
+//		configurer.setLocations(
+//				new ClassPathResource("arsnova.properties.example"),
+//				new FileSystemResource("/etc/arsnova/arsnova.properties")
+//		);
+//		configurer.setIgnoreResourceNotFound(true);
+//		configurer.setIgnoreUnresolvablePlaceholders(false);
+//
+//		return configurer;
+//	}
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+		final PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+//		configurer.setLocations(
+//			new ClassPathResource("arsnova.properties.example"),
+//			new FileSystemResource("/etc/arsnova/arsnova.properties")
+//		);
+//		configurer.setIgnoreResourceNotFound(true);
+		configurer.setIgnoreUnresolvablePlaceholders(false);
+
+		return configurer;
+	}
+
+	@Bean
+	public PropertiesFactoryBean versionInfoProperties() {
+		PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
+		propertiesFactoryBean.setLocation(new ClassPathResource("version.properties"));
+
+		return propertiesFactoryBean;
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		return new CorsFilter(Arrays.asList(corsOrigins));
+	}
+
+	@Bean(name = "connectorClient")
+	public ConnectorClient connectorClient() {
+		if (!connectorEnable) {
+			return null;
+		}
+
+		final ConnectorClientImpl connectorClient = new ConnectorClientImpl();
+		connectorClient.setServiceLocation(connectorUri);
+		connectorClient.setUsername(connectorUsername);
+		connectorClient.setPassword(connectorPassword);
+		return connectorClient;
+	}
+
+	@Profile("!test")
+	@Bean(name = "socketServer", initMethod = "startServer", destroyMethod = "stopServer")
+	public ARSnovaSocket socketServer() {
+		final ARSnovaSocketIOServer socketServer = new ARSnovaSocketIOServer();
+		socketServer.setHostIp(socketAddress);
+		socketServer.setPortNumber(socketPort);
+		socketServer.setUseSSL(!socketKeystore.isEmpty());
+		socketServer.setKeystore(socketKeystore);
+		socketServer.setStorepass(socketKeystorePassword);
+		return socketServer;
+	}
+
+	@Bean
+	public CacheManager cacheManager() {
+		return new ConcurrentMapCacheManager();
+	}
+
+	@Bean
+	public JavaMailSenderImpl mailSender() {
+		final JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setHost(mailHost);
+
+		return mailSender;
+	}
+
+	@Bean
+	public ImageUtils imageUtils() {
+		return new ImageUtils();
+	}
+}

src/main/java/de/thm/arsnova/socket/ARSnovaSocketListener.java
@@ -21,13 +21,13 @@ import de.thm.arsnova.events.NovaEvent;
 import de.thm.arsnova.events.NovaEventVisitor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 /**
  * An external Listener is required because otherwise the event methods are not called through a Spring proxy.
  * This would result in Spring method annotations not working.
  */
-@Component
+@Service
 public class ARSnovaSocketListener implements ApplicationListener<NovaEvent> {
 
 	@Autowired

src/main/java/de/thm/arsnova/aop/UserSessionAspect.java
@@ -23,12 +23,14 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * Assigns a session to the {@link de.thm.arsnova.services.UserSessionService} whenever a user joins a
  * session.
  */
 @Aspect
+@Service
 public class UserSessionAspect {
 
 	@Autowired

src/main/java/de/thm/arsnova/config/ExtraConfig.java
@@ -1,140 +1,0 @@
-/*
- * This file is part of ARSnova Backend.
- * Copyright (C) 2012-2017 The ARSnova Team
- *
- * ARSnova Backend is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * ARSnova Backend is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-package de.thm.arsnova.config;
-
-import de.thm.arsnova.ImageUtils;
-import de.thm.arsnova.connector.client.ConnectorClient;
-import de.thm.arsnova.connector.client.ConnectorClientImpl;
-import de.thm.arsnova.socket.ARSnovaSocket;
-import de.thm.arsnova.socket.ARSnovaSocketIOServer;
-import de.thm.arsnova.web.CorsFilter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.config.PropertiesFactoryBean;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import java.util.Arrays;
-
-/**
- * Loads property file and configures non-security related beans and components.
- */
-@EnableWebMvc
-@Configuration
-@EnableCaching
-public class ExtraConfig extends WebMvcConfigurerAdapter {
-
-	@Autowired
-	private Environment env;
-
-	@Value(value = "${connector.enable}") private boolean connectorEnable;
-	@Value(value = "${connector.uri}") private String connectorUri;
-	@Value(value = "${connector.username}") private String connectorUsername;
-	@Value(value = "${connector.password}") private String connectorPassword;
-
-	@Value(value = "${socketio.bind-address}") private String socketAddress;
-	@Value(value = "${socketio.port}") private int socketPort;
-	@Value(value = "${socketio.ssl.jks-file:}") private String socketKeystore;
-	@Value(value = "${socketio.ssl.jks-password:}") private String socketKeystorePassword;
-	@Value(value = "${security.cors.origins:}") private String[] corsOrigins;
-
-	private static int testPortOffset = 0;
-
-	@Bean
-	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-		final PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
-		configurer.setLocations(
-			new ClassPathResource("arsnova.properties.example"),
-			new FileSystemResource("/etc/arsnova/arsnova.properties")
-		);
-		configurer.setIgnoreResourceNotFound(true);
-		configurer.setIgnoreUnresolvablePlaceholders(false);
-		return configurer;
-	}
-
-	@Bean
-	public PropertiesFactoryBean versionInfoProperties() {
-		PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
-		propertiesFactoryBean.setLocation(new ClassPathResource("version.properties"));
-
-		return propertiesFactoryBean;
-	}
-
-	@Bean
-	public CorsFilter corsFilter() {
-		return new CorsFilter(Arrays.asList(corsOrigins));
-	}
-
-	@Bean(name = "connectorClient")
-	public ConnectorClient connectorClient() {
-		if (!connectorEnable) {
-			return null;
-		}
-
-		final ConnectorClientImpl connectorClient = new ConnectorClientImpl();
-		connectorClient.setServiceLocation(connectorUri);
-		connectorClient.setUsername(connectorUsername);
-		connectorClient.setPassword(connectorPassword);
-		return connectorClient;
-	}
-
-	@Profile("!test")
-	@Bean(name = "socketServer", initMethod = "startServer", destroyMethod = "stopServer")
-	public ARSnovaSocket socketServer() {
-		final ARSnovaSocketIOServer socketServer = new ARSnovaSocketIOServer();
-		socketServer.setHostIp(socketAddress);
-		socketServer.setPortNumber(socketPort);
-		socketServer.setUseSSL(!socketKeystore.isEmpty());
-		socketServer.setKeystore(socketKeystore);
-		socketServer.setStorepass(socketKeystorePassword);
-		return socketServer;
-	}
-
-	@Profile("test")
-	@Bean(name = "socketServer", initMethod = "startServer", destroyMethod = "stopServer")
-	public ARSnovaSocket socketTestServer() {
-		final int testSocketPort = 1234 + testPortOffset++ % 10;
-		final ARSnovaSocketIOServer socketServer = new ARSnovaSocketIOServer();
-		socketServer.setHostIp(socketAddress);
-		socketServer.setPortNumber(socketPort + testSocketPort);
-		socketServer.setUseSSL(!socketKeystore.isEmpty());
-		socketServer.setKeystore(socketKeystore);
-		socketServer.setStorepass(socketKeystorePassword);
-		return socketServer;
-	}
-
-	@Bean
-	public CacheManager cacheManager() {
-		return new ConcurrentMapCacheManager();
-	}
-
-	@Bean
-	public ImageUtils imageUtils() {
-		return new ImageUtils();
-	}
-}

src/main/java/de/thm/arsnova/dao/CouchDBDao.java
@@ -51,8 +51,9 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.Caching;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.ApplicationEventPublisherAware;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -79,7 +80,8 @@ import java.util.concurrent.ConcurrentLinkedQueue;
  * @see <a href="http://docs.spring.io/spring/docs/current/spring-framework-reference/html/cache.html">Spring Framework's Cache Abstraction</a>
  * @see <a href="https://github.com/thm-projects/arsnova-backend/wiki/Caching">Caching in ARSnova explained</a>
  */
-@Component("databaseDao")
+@Profile("!test")
+@Service("databaseDao")
 public class CouchDBDao implements IDatabaseDao, ApplicationEventPublisherAware {
 
 	private static final int BULK_PARTITION_SIZE = 500;

src/main/webapp/WEB-INF/arsnova-servlet.xml
@@ -1,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<beans xmlns = "http://www.springframework.org/schema/beans"
+	   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+	   xsi:schemaLocation="http://www.springframework.org/schema/beans http://www.springframework.org/schema/beans/spring-beans-4.3.xsd">
+</beans>

src/main/webapp/WEB-INF/spring/arsnova-servlet.xml
@@ -1,103 +1,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<beans xmlns="http://www.springframework.org/schema/beans"
-	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-	xmlns:aop="http://www.springframework.org/schema/aop"
-	xmlns:context="http://www.springframework.org/schema/context"
-	xmlns:mvc="http://www.springframework.org/schema/mvc"
-	xmlns:p="http://www.springframework.org/schema/p"
-	xsi:schemaLocation="
-		http://www.springframework.org/schema/aop http://www.springframework.org/schema/aop/spring-aop-4.3.xsd
-		http://www.springframework.org/schema/beans http://www.springframework.org/schema/beans/spring-beans-4.3.xsd
-		http://www.springframework.org/schema/context http://www.springframework.org/schema/context/spring-context-4.3.xsd
-		http://www.springframework.org/schema/mvc http://www.springframework.org/schema/mvc/spring-mvc-4.3.xsd">
-
-	<!-- ARSnova Servlet Context -->
-
-	<context:component-scan base-package="de.thm.arsnova.aop,de.thm.arsnova.controller,de.thm.arsnova.web" />
-
-	<mvc:annotation-driven
-		content-negotiation-manager="mvcContentNegotiationManager" />
-
-	<mvc:interceptors>
-		<bean class="de.thm.arsnova.web.CacheControlInterceptorHandler" />
-		<bean class="de.thm.arsnova.web.DeprecatedApiInterceptorHandler" />
-		<bean class="de.thm.arsnova.web.ResponseInterceptorHandler" />
-	</mvc:interceptors>
-
-	<bean id="requestMappingHandlerMapping" class="org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping" primary="true"></bean>
-
-	<aop:aspectj-autoproxy />
-
-	<!-- Enables swgger ui-->
-	<mvc:resources mapping="swagger.json" location="classpath:/"/>
-
-	<bean id="propertyPlaceholderConfigurer"
-		class="org.springframework.beans.factory.config.PropertyPlaceholderConfigurer"
-		p:ignoreUnresolvablePlaceholders="false" p:ignoreResourceNotFound="true">
-		<property name="locations">
-			<list>
-				<value>classpath:arsnova.properties.example</value>
-				<value>file:///etc/arsnova/arsnova.properties</value>
-			</list>
-		</property>
-		<property name="fileEncoding" value="UTF-8" />
-	</bean>
-
-	<bean id="mvcContentNegotiationManager"
-		class="org.springframework.web.accept.ContentNegotiationManagerFactoryBean">
-		<property name="favorPathExtension" value="false" />
-		<property name="favorParameter" value="true" />
-		<property name="defaultContentType" value="application/json" />
-		<property name="mediaTypes">
-			<value>
-				html=text/html
-				json=application/json
-				xml=application/xml
-			</value>
-		</property>
-	</bean>
-
-	<mvc:annotation-driven />
-	<mvc:resources mapping="/**" location="/" />
-
-	<!-- -->
-	<bean
-		class="org.springframework.web.servlet.mvc.annotation.DefaultAnnotationHandlerMapping" />
-	<bean
-		class="org.springframework.web.servlet.mvc.annotation.AnnotationMethodHandlerAdapter">
-		<property name="messageConverters">
-			<list>
-				<bean
-					class="org.springframework.http.converter.StringHttpMessageConverter">
-					<property name="supportedMediaTypes" value="text/plain;charset=UTF-8" />
-				</bean>
-				<bean
-					class="org.springframework.http.converter.json.MappingJackson2HttpMessageConverter" />
-			</list>
-		</property>
-	</bean>
-	<!-- -->
-
-	<bean
-		class="org.springframework.web.servlet.view.ContentNegotiatingViewResolver">
-		<property name="viewResolvers">
-			<list>
-				<bean class="org.springframework.web.servlet.view.UrlBasedViewResolver">
-					<property name="viewClass"
-						value="org.springframework.web.servlet.view.JstlView" />
-					<property name="prefix" value="/WEB-INF/jsp/" />
-					<property name="suffix" value=".jsp" />
-				</bean>
-			</list>
-		</property>
-		<property name="defaultViews">
-			<list>
-				<bean
-					class="org.springframework.web.servlet.view.json.MappingJackson2JsonView">
-					<property name="prefixJson" value="false" />
-				</bean>
-			</list>
-		</property>
-	</bean>
-
-</beans>

src/main/webapp/WEB-INF/spring/spring-main.xml
@@ -1,50 +1,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<beans xmlns="http://www.springframework.org/schema/beans"
-	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-	xmlns:aop="http://www.springframework.org/schema/aop"
-	xmlns:context="http://www.springframework.org/schema/context"
-	xmlns:task="http://www.springframework.org/schema/task"
-	xmlns:p="http://www.springframework.org/schema/p"
-	xsi:schemaLocation="
-		http://www.springframework.org/schema/aop http://www.springframework.org/schema/aop/spring-aop-4.3.xsd
-		http://www.springframework.org/schema/beans http://www.springframework.org/schema/beans/spring-beans-4.3.xsd
-		http://www.springframework.org/schema/context http://www.springframework.org/schema/context/spring-context-4.3.xsd
-		http://www.springframework.org/schema/task http://www.springframework.org/schema/task/spring-task-4.3.xsd">
-
-	<bean id="propertyPlaceholderConfigurer"
-		class="org.springframework.beans.factory.config.PropertyPlaceholderConfigurer"
-		p:ignoreUnresolvablePlaceholders="false" p:ignoreResourceNotFound="true">
-		<property name="locations">
-			<list>
-				<value>classpath:arsnova.properties.example</value>
-				<value>file:///etc/arsnova/arsnova.properties</value>
-			</list>
-		</property>
-		<property name="fileEncoding" value="UTF-8" />
-	</bean>
-
-	<context:component-scan base-package="de.thm.arsnova.dao,de.thm.arsnova.cache,de.thm.arsnova.events,de.thm.arsnova.security,de.thm.arsnova.services,de.thm.arsnova.config,de.thm.arsnova.domain" />
-
-	<context:annotation-config />
-
-	<task:annotation-driven />
-
-	<!--
-		expose-proxy is needed to access the proxy object via AopContext.currentProxy() in CouchDBDao. It might have a
-		negative impact on performance but is needed for caching until a better solution is implemented (e.g. use of
-		AspectJ's weaving).
-	-->
-	<aop:aspectj-autoproxy expose-proxy="true">
-		<aop:include name="userSessionAspect" />
-	</aop:aspectj-autoproxy>
-
-	<bean id="userSessionAspect" class="de.thm.arsnova.aop.UserSessionAspect" />
-
-	<bean id="mailSender" class="org.springframework.mail.javamail.JavaMailSenderImpl">
-		<property name="host" value="${mail.host}"/>
-	</bean>
-
-	<bean id="databaseDao" class="de.thm.arsnova.dao.CouchDBDao" />
-
-	<bean id="socketListener" class="de.thm.arsnova.socket.ARSnovaSocketListener" />
-</beans>

commit 402bf37504f874dae3d311c3da7e497450537b27

Fix registration of Bean for socket event handling
Fixes a regression introduced by commit
2ecbfec8b1676781a57f8622908af7cc44abfd48.

src/main/java/de/thm/arsnova/config/AppConfig.java
@@ -32,6 +32,7 @@ import de.thm.arsnova.persistance.couchdb.*;
 import de.thm.arsnova.persistance.couchdb.InitializingCouchDbConnector;
 import de.thm.arsnova.socket.ARSnovaSocket;
 import de.thm.arsnova.socket.ARSnovaSocketIOServer;
+import de.thm.arsnova.socket.ARSnovaSocketListener;
 import de.thm.arsnova.web.CacheControlInterceptorHandler;
 import de.thm.arsnova.web.CorsFilter;
 import de.thm.arsnova.web.DeprecatedApiInterceptorHandler;
@@ -268,6 +269,11 @@ public class AppConfig extends WebMvcConfigurerAdapter {
 		socketServer.setKeystore(socketKeystore);
 		socketServer.setStorepass(socketKeystorePassword);
 		return socketServer;
+	}
+
+	@Bean
+	public ARSnovaSocketListener arsnovaSocketListener() {
+		return new ARSnovaSocketListener();
 	}
 
 	@Bean

src/main/java/de/thm/arsnova/socket/ARSnovaSocketListener.java
@@ -21,13 +21,13 @@ import de.thm.arsnova.events.NovaEvent;
 import de.thm.arsnova.events.NovaEventVisitor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 /**
  * An external Listener is required because otherwise the event methods are not called through a Spring proxy.
  * This would result in Spring method annotations not working.
  */
-@Service
+@Component
 public class ARSnovaSocketListener implements ApplicationListener<NovaEvent> {
 
 	@Autowired