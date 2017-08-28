sucof/arsnova-backend

Refactor package and class hierarchy
* Moved classes from `de.thm.arsnova` root package.
* New util package
* Do not start interface names with `I`. Appended `Impl` to the
  implementing class instead.
* Renamed learning progress classes to score and move them from
  `.domain` to a new `.services.score` package.
* Do not capitalize acronyms in class names:
    * CASLogoutSuccessHandler -> CasLogoutSuccessHandler
    * SocketIO* -> Socketio*
* Renamed `.socket` package to `.websocket`

src/main/java/de/thm/arsnova/cache/CacheBusterImpl.java
@@ -1,0 +1,130 @@
+/*
+ * This file is part of ARSnova Backend.
+ * Copyright (C) 2012-2017 The ARSnova Team
+ *
+ * ARSnova Backend is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ARSnova Backend is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.thm.arsnova.cache;
+
+import de.thm.arsnova.events.*;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.stereotype.Component;
+
+/**
+ * This class is used to evict caches based on events. The events carry all necessary information to clear the
+ * caches, e.g, for a specific session.
+ */
+@Component
+public class CacheBusterImpl implements CacheBuster, ArsnovaEventVisitor {
+
+	@CacheEvict(value = "statistics", allEntries = true)
+	@Override
+	public void visit(NewCommentEvent event) { }
+
+	@CacheEvict(value = "statistics", allEntries = true)
+	@Override
+	public void visit(DeleteCommentEvent event) { }
+
+	@Override
+	public void visit(NewQuestionEvent event) { }
+
+	@Override
+	public void visit(UnlockQuestionEvent event) { }
+
+	@Override
+	public void visit(UnlockQuestionsEvent newQuestionsEvent) { }
+
+	@Override
+	public void visit(LockQuestionEvent lockQuestionEvent) { }
+
+	@Override
+	public void visit(LockQuestionsEvent lockQuestionsEvent) { }
+
+	@CacheEvict(value = "answers", key = "#event.content")
+	@Override
+	public void visit(NewAnswerEvent event) { }
+
+	@Override
+	public void visit(DeleteAnswerEvent event) { }
+
+	@Override
+	public void visit(DeleteQuestionEvent event) { }
+
+	@Override
+	public void visit(DeleteAllQuestionsEvent event) { }
+
+	@Override
+	public void visit(DeleteAllQuestionsAnswersEvent event) { }
+
+	@Override
+	public void visit(DeleteAllPreparationAnswersEvent event) { }
+
+	@Override
+	public void visit(DeleteAllLectureAnswersEvent event) { }
+
+	@Override
+	public void visit(NewFeedbackEvent event) { }
+
+	@Override
+	public void visit(DeleteFeedbackForSessionsEvent event) { }
+
+	@Override
+	public void visit(StatusSessionEvent event) { }
+
+	@CacheEvict(value = "statistics", allEntries = true)
+	@Override
+	public void visit(ChangeLearningProgressEvent changeLearningProgress) { }
+
+	@Override
+	public void visit(PiRoundDelayedStartEvent piRoundDelayedStartEvent) { }
+
+	@Override
+	public void visit(PiRoundEndEvent piRoundEndEvent) { }
+
+	@Override
+	public void visit(PiRoundCancelEvent piRoundCancelEvent) { }
+
+	@Override
+	public void visit(PiRoundResetEvent piRoundResetEvent) { }
+
+	@CacheEvict(value = "statistics", allEntries = true)
+	@Override
+	public void visit(NewSessionEvent newSessionEvent) { }
+
+	@CacheEvict(value = "statistics", allEntries = true)
+	@Override
+	public void visit(DeleteSessionEvent deleteSessionEvent) { }
+
+	@Override
+	public void visit(LockVoteEvent lockVoteEvent) { }
+
+	@Override
+	public void visit(LockVotesEvent lockVotesEvent) { }
+
+	@Override
+	public void visit(UnlockVoteEvent unlockVoteEvent) { }
+
+	@Override
+	public void visit(UnlockVotesEvent unlockVotesEvent) { }
+
+	@Override
+	public void visit(FeatureChangeEvent featureChangeEvent) { }
+
+	@Override
+	public void visit(LockFeedbackEvent lockFeedbackEvent) { }
+
+	@Override
+	public void visit(FlipFlashcardsEvent flipFlashcardsEvent) { }
+
+}

src/main/java/de/thm/arsnova/entities/transport/ScoreOptions.java
@@ -1,0 +1,61 @@
+/*
+ * This file is part of ARSnova Backend.
+ * Copyright (C) 2012-2017 The ARSnova Team
+ *
+ * ARSnova Backend is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ARSnova Backend is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.thm.arsnova.entities.transport;
+
+/**
+ * A session's settings regarding the calculation of the learning progress.
+ */
+public class ScoreOptions {
+
+	private String sessionKeyword;
+
+	private String type;
+
+	private String questionVariant;
+
+	public String getSessionKeyword() {
+		return sessionKeyword;
+	}
+
+	public void setSessionKeyword(String sessionKeyword) {
+		this.sessionKeyword = sessionKeyword;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String learningProgressType) {
+		this.type = learningProgressType;
+	}
+
+	public String getQuestionVariant() {
+		return questionVariant;
+	}
+
+	public void setQuestionVariant(String questionVariant) {
+		this.questionVariant = questionVariant;
+	}
+
+	public de.thm.arsnova.entities.ScoreOptions toEntity() {
+		de.thm.arsnova.entities.ScoreOptions entity = new de.thm.arsnova.entities.ScoreOptions();
+		entity.setType(this.getType());
+		entity.setQuestionVariant(this.getQuestionVariant());
+		return entity;
+	}
+}

src/main/java/de/thm/arsnova/events/ArsnovaEventVisitor.java
@@ -1,0 +1,86 @@
+/*
+ * This file is part of ARSnova Backend.
+ * Copyright (C) 2012-2017 The ARSnova Team
+ *
+ * ARSnova Backend is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ARSnova Backend is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.thm.arsnova.events;
+
+/**
+ * Listeners wanting to receive ARSnova's internal events should implement this interface.
+ */
+public interface ArsnovaEventVisitor {
+
+	void visit(NewCommentEvent newCommentEvent);
+
+	void visit(DeleteCommentEvent deleteCommentEvent);
+
+	void visit(NewQuestionEvent newQuestionEvent);
+
+	void visit(UnlockQuestionEvent unlockQuestionEvent);
+
+	void visit(UnlockQuestionsEvent newQuestionsEvent);
+
+	void visit(LockQuestionEvent lockQuestionEvent);
+
+	void visit(LockQuestionsEvent lockQuestionsEvent);
+
+	void visit(NewAnswerEvent newAnswerEvent);
+
+	void visit(DeleteAnswerEvent deleteAnswerEvent);
+
+	void visit(DeleteQuestionEvent deleteQuestionEvent);
+
+	void visit(DeleteAllQuestionsEvent deleteAllQuestionsEvent);
+
+	void visit(DeleteAllQuestionsAnswersEvent deleteAllAnswersEvent);
+
+	void visit(DeleteAllPreparationAnswersEvent deleteAllPreparationAnswersEvent);
+
+	void visit(DeleteAllLectureAnswersEvent deleteAllLectureAnswersEvent);
+
+	void visit(NewFeedbackEvent newFeedbackEvent);
+
+	void visit(DeleteFeedbackForSessionsEvent deleteFeedbackEvent);
+
+	void visit(StatusSessionEvent statusSessionEvent);
+
+	void visit(ChangeLearningProgressEvent changeLearningProgress);
+
+	void visit(PiRoundDelayedStartEvent piRoundDelayedStartEvent);
+
+	void visit(PiRoundEndEvent piRoundEndEvent);
+
+	void visit(PiRoundCancelEvent piRoundCancelEvent);
+
+	void visit(PiRoundResetEvent piRoundResetEvent);
+
+	void visit(NewSessionEvent newSessionEvent);
+
+	void visit(DeleteSessionEvent deleteSessionEvent);
+
+	void visit(LockVoteEvent lockVoteEvent);
+
+	void visit(LockVotesEvent lockVotesEvent);
+
+	void visit(UnlockVoteEvent unlockVoteEvent);
+
+	void visit(UnlockVotesEvent unlockVotesEvent);
+
+	void visit(FeatureChangeEvent featureChangeEvent);
+
+	void visit(LockFeedbackEvent lockFeedbackEvent);
+
+	void visit(FlipFlashcardsEvent flipFlashcardsEvent);
+}

particify/arsnova-server

src/main/java/de/thm/arsnova/cache/CacheBusterImpl.java
@@ -1,0 +1,130 @@
+/*
+ * This file is part of ARSnova Backend.
+ * Copyright (C) 2012-2017 The ARSnova Team
+ *
+ * ARSnova Backend is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ARSnova Backend is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.thm.arsnova.cache;
+
+import de.thm.arsnova.events.*;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.stereotype.Component;
+
+/**
+ * This class is used to evict caches based on events. The events carry all necessary information to clear the
+ * caches, e.g, for a specific session.
+ */
+@Component
+public class CacheBusterImpl implements CacheBuster, ArsnovaEventVisitor {
+
+	@CacheEvict(value = "statistics", allEntries = true)
+	@Override
+	public void visit(NewCommentEvent event) { }
+
+	@CacheEvict(value = "statistics", allEntries = true)
+	@Override
+	public void visit(DeleteCommentEvent event) { }
+
+	@Override
+	public void visit(NewQuestionEvent event) { }
+
+	@Override
+	public void visit(UnlockQuestionEvent event) { }
+
+	@Override
+	public void visit(UnlockQuestionsEvent newQuestionsEvent) { }
+
+	@Override
+	public void visit(LockQuestionEvent lockQuestionEvent) { }
+
+	@Override
+	public void visit(LockQuestionsEvent lockQuestionsEvent) { }
+
+	@CacheEvict(value = "answers", key = "#event.content")
+	@Override
+	public void visit(NewAnswerEvent event) { }
+
+	@Override
+	public void visit(DeleteAnswerEvent event) { }
+
+	@Override
+	public void visit(DeleteQuestionEvent event) { }
+
+	@Override
+	public void visit(DeleteAllQuestionsEvent event) { }
+
+	@Override
+	public void visit(DeleteAllQuestionsAnswersEvent event) { }
+
+	@Override
+	public void visit(DeleteAllPreparationAnswersEvent event) { }
+
+	@Override
+	public void visit(DeleteAllLectureAnswersEvent event) { }
+
+	@Override
+	public void visit(NewFeedbackEvent event) { }
+
+	@Override
+	public void visit(DeleteFeedbackForSessionsEvent event) { }
+
+	@Override
+	public void visit(StatusSessionEvent event) { }
+
+	@CacheEvict(value = "statistics", allEntries = true)
+	@Override
+	public void visit(ChangeScoreEvent changeLearningProgress) { }
+
+	@Override
+	public void visit(PiRoundDelayedStartEvent piRoundDelayedStartEvent) { }
+
+	@Override
+	public void visit(PiRoundEndEvent piRoundEndEvent) { }
+
+	@Override
+	public void visit(PiRoundCancelEvent piRoundCancelEvent) { }
+
+	@Override
+	public void visit(PiRoundResetEvent piRoundResetEvent) { }
+
+	@CacheEvict(value = "statistics", allEntries = true)
+	@Override
+	public void visit(NewSessionEvent newSessionEvent) { }
+
+	@CacheEvict(value = "statistics", allEntries = true)
+	@Override
+	public void visit(DeleteSessionEvent deleteSessionEvent) { }
+
+	@Override
+	public void visit(LockVoteEvent lockVoteEvent) { }
+
+	@Override
+	public void visit(LockVotesEvent lockVotesEvent) { }
+
+	@Override
+	public void visit(UnlockVoteEvent unlockVoteEvent) { }
+
+	@Override
+	public void visit(UnlockVotesEvent unlockVotesEvent) { }
+
+	@Override
+	public void visit(FeatureChangeEvent featureChangeEvent) { }
+
+	@Override
+	public void visit(LockFeedbackEvent lockFeedbackEvent) { }
+
+	@Override
+	public void visit(FlipFlashcardsEvent flipFlashcardsEvent) { }
+
+}

src/main/java/de/thm/arsnova/entities/transport/ScoreOptions.java
@@ -1,0 +1,61 @@
+/*
+ * This file is part of ARSnova Backend.
+ * Copyright (C) 2012-2017 The ARSnova Team
+ *
+ * ARSnova Backend is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ARSnova Backend is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.thm.arsnova.entities.transport;
+
+/**
+ * A session's settings regarding the calculation of the score.
+ */
+public class ScoreOptions {
+
+	private String sessionKeyword;
+
+	private String type;
+
+	private String questionVariant;
+
+	public String getSessionKeyword() {
+		return sessionKeyword;
+	}
+
+	public void setSessionKeyword(String sessionKeyword) {
+		this.sessionKeyword = sessionKeyword;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getQuestionVariant() {
+		return questionVariant;
+	}
+
+	public void setQuestionVariant(String questionVariant) {
+		this.questionVariant = questionVariant;
+	}
+
+	public de.thm.arsnova.entities.ScoreOptions toEntity() {
+		de.thm.arsnova.entities.ScoreOptions entity = new de.thm.arsnova.entities.ScoreOptions();
+		entity.setType(this.getType());
+		entity.setQuestionVariant(this.getQuestionVariant());
+		return entity;
+	}
+}

src/main/java/de/thm/arsnova/events/ArsnovaEventVisitor.java
@@ -1,0 +1,86 @@
+/*
+ * This file is part of ARSnova Backend.
+ * Copyright (C) 2012-2017 The ARSnova Team
+ *
+ * ARSnova Backend is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ARSnova Backend is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.thm.arsnova.events;
+
+/**
+ * Listeners wanting to receive ARSnova's internal events should implement this interface.
+ */
+public interface ArsnovaEventVisitor {
+
+	void visit(NewCommentEvent newCommentEvent);
+
+	void visit(DeleteCommentEvent deleteCommentEvent);
+
+	void visit(NewQuestionEvent newQuestionEvent);
+
+	void visit(UnlockQuestionEvent unlockQuestionEvent);
+
+	void visit(UnlockQuestionsEvent newQuestionsEvent);
+
+	void visit(LockQuestionEvent lockQuestionEvent);
+
+	void visit(LockQuestionsEvent lockQuestionsEvent);
+
+	void visit(NewAnswerEvent newAnswerEvent);
+
+	void visit(DeleteAnswerEvent deleteAnswerEvent);
+
+	void visit(DeleteQuestionEvent deleteQuestionEvent);
+
+	void visit(DeleteAllQuestionsEvent deleteAllQuestionsEvent);
+
+	void visit(DeleteAllQuestionsAnswersEvent deleteAllAnswersEvent);
+
+	void visit(DeleteAllPreparationAnswersEvent deleteAllPreparationAnswersEvent);
+
+	void visit(DeleteAllLectureAnswersEvent deleteAllLectureAnswersEvent);
+
+	void visit(NewFeedbackEvent newFeedbackEvent);
+
+	void visit(DeleteFeedbackForSessionsEvent deleteFeedbackEvent);
+
+	void visit(StatusSessionEvent statusSessionEvent);
+
+	void visit(ChangeScoreEvent changeLearningProgress);
+
+	void visit(PiRoundDelayedStartEvent piRoundDelayedStartEvent);
+
+	void visit(PiRoundEndEvent piRoundEndEvent);
+
+	void visit(PiRoundCancelEvent piRoundCancelEvent);
+
+	void visit(PiRoundResetEvent piRoundResetEvent);
+
+	void visit(NewSessionEvent newSessionEvent);
+
+	void visit(DeleteSessionEvent deleteSessionEvent);
+
+	void visit(LockVoteEvent lockVoteEvent);
+
+	void visit(LockVotesEvent lockVotesEvent);
+
+	void visit(UnlockVoteEvent unlockVoteEvent);
+
+	void visit(UnlockVotesEvent unlockVotesEvent);
+
+	void visit(FeatureChangeEvent featureChangeEvent);
+
+	void visit(LockFeedbackEvent lockFeedbackEvent);
+
+	void visit(FlipFlashcardsEvent flipFlashcardsEvent);
+}