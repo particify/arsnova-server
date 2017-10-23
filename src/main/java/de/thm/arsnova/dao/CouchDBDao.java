particify/arsnova-server

commit dd749370abe8453056c69a587988b6b20e6a5a68

add motdimport to sessionimport (added in bulk save) add empty test methods

src/main/java/de/thm/arsnova/dao/CouchDBDao.java
@@ -2117,6 +2117,8 @@ public class CouchDBDao implements IDatabaseDao, ApplicationEventPublisherAware 
 		List<Document> answers = new ArrayList<Document>();
 		// We can then push answers together with interposed questions in one large bulk request
 		List<Document> interposedQuestions = new ArrayList<Document>();
+		// Motds shouldn't be forgotten
+		List<Document> motds = new ArrayList<Document>();
 		try {
 			// add session id to all questions and generate documents
 			for (ImportExportQuestion question : importSession.getQuestions()) {
@@ -2163,8 +2165,20 @@ public class CouchDBDao implements IDatabaseDao, ApplicationEventPublisherAware 
 				q.put("creator", "");
 				interposedQuestions.add(q);
 			}
+			for (Motd m : importSession.getMotds()) {
+				final Document d = new Document();
+				d.put("type", "motd");
+				d.put("motdkey", m.getMotdkey());
+				d.put("title", m.getTitle());
+				d.put("audience", m.getAudience());
+				d.put("sessionkey", session.getKeyword());
+				d.put("startdate", String.valueOf(m.getStartdate()));
+				d.put("enddate", String.valueOf(m.getEnddate()));
+				motds.add(d);
+			}
 			List<Document> documents = new ArrayList<Document>(answers);
 			documents.addAll(interposedQuestions);
+			documents.addAll(motds);
 			database.bulkSaveDocuments(documents.toArray(new Document[documents.size()]));
 		} catch (IOException e) {
 			LOGGER.error("Could not import this session: {}", e.getMessage());

src/test/java/de/thm/arsnova/dao/StubDatabaseDao.java
@@ -502,6 +502,12 @@ public class StubDatabaseDao implements IDatabaseDao {
 	}
 
 	@Override
+	public ImportExportSession exportSession(String sessionkey) {
+		// TODO Auto.generated method stub
+		return null;
+	}
+
+	@Override
 	public List<Question> getSkillQuestionsForUsers(Session session) {
 		// TODO Auto-generated method stub
 		return null;
@@ -545,6 +551,12 @@ public class StubDatabaseDao implements IDatabaseDao {
 
 	@Override
 	public List<Question> getPreparationQuestionsForTeachers(Session session) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Question> getAllSkillQuestions(Session session) {
 		// TODO Auto-generated method stub
 		return null;
 	}

commit 63603e108c9f67b7b8f7194567c1cd58efa937e2

Fix NPE on session import w/o MotD data
Fixes a regression introduced with commit
dd749370abe8453056c69a587988b6b20e6a5a68.

Refs arsnova/arsnova#85
Refs !16

src/main/java/de/thm/arsnova/dao/CouchDBDao.java
@@ -2447,17 +2447,19 @@ public class CouchDBDao implements IDatabaseDao, ApplicationEventPublisherAware 
 				q.put("creator", "");
 				interposedQuestions.add(q);
 			}
-			for (Motd m : importSession.getMotds()) {
-				final Document d = new Document();
-				d.put("type", "motd");
-				d.put("motdkey", m.getMotdkey());
-				d.put("title", m.getTitle());
-				d.put("text", m.getText());
-				d.put("audience", m.getAudience());
-				d.put("sessionkey", session.getKeyword());
-				d.put("startdate", String.valueOf(m.getStartdate().getTime()));
-				d.put("enddate", String.valueOf(m.getEnddate().getTime()));
-				motds.add(d);
+			if (importSession.getMotds() != null) {
+				for (Motd m : importSession.getMotds()) {
+					final Document d = new Document();
+					d.put("type", "motd");
+					d.put("motdkey", m.getMotdkey());
+					d.put("title", m.getTitle());
+					d.put("text", m.getText());
+					d.put("audience", m.getAudience());
+					d.put("sessionkey", session.getKeyword());
+					d.put("startdate", String.valueOf(m.getStartdate().getTime()));
+					d.put("enddate", String.valueOf(m.getEnddate().getTime()));
+					motds.add(d);
+				}
 			}
 			List<Document> documents = new ArrayList<>(answers);
 			database.bulkSaveDocuments(interposedQuestions.toArray(new Document[interposedQuestions.size()]));