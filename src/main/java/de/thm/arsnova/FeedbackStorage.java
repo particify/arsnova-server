particify/arsnova-server

commit 7207bad9d74ef23af14012a2440cb6ae5a394af5

Add "liveClicker" use case for abcd-live-questions.
Feedback reset will be disabled for liveClicker-sessions.

src/main/java/de/thm/arsnova/FeedbackStorage.java
@@ -119,15 +119,18 @@ public class FeedbackStorage {
 	public Map<Session, List<User>> cleanFeedbackVotes(final int cleanupFeedbackDelay) {
 		final Map<Session, List<User>> removedFeedbackOfUsersInSession = new HashMap<Session, List<User>>();
 		for (final Session session : data.keySet()) {
-			List<User> affectedUsers = cleanFeedbackVotesInSession(session, cleanupFeedbackDelay);
-			if (!affectedUsers.isEmpty()) {
-				removedFeedbackOfUsersInSession.put(session, affectedUsers);
+			if (!session.getFeatures().isLiveClicker()) {
+				List<User> affectedUsers = cleanFeedbackVotesInSession(session, cleanupFeedbackDelay);
+				if (!affectedUsers.isEmpty()) {
+					removedFeedbackOfUsersInSession.put(session, affectedUsers);
+				}
 			}
 		}
 		return removedFeedbackOfUsersInSession;
 	}
 
-	private List<User> cleanFeedbackVotesInSession(final Session session, final int cleanupFeedbackDelayInMins) {
+	@Transactional(isolation = Isolation.READ_COMMITTED)
+	public List<User> cleanFeedbackVotesInSession(final Session session, final int cleanupFeedbackDelayInMins) {
 		final long timelimitInMillis = TimeUnit.MILLISECONDS.convert(cleanupFeedbackDelayInMins, TimeUnit.MINUTES);
 		final Date maxAllowedTime = new Date(System.currentTimeMillis() - timelimitInMillis);
 

src/main/java/de/thm/arsnova/entities/SessionFeature.java
@@ -29,6 +29,7 @@ public class SessionFeature {
 	private boolean clicker = false;
 	private boolean peerGrading = false;
 	private boolean liveFeedback = false;
+	private boolean liveClicker = false;
 	private boolean flashcard = false;
 	private boolean total = false;
 
@@ -46,6 +47,7 @@ public class SessionFeature {
 			this.clicker = features.clicker;
 			this.peerGrading = features.peerGrading;
 			this.liveFeedback = features.liveFeedback;
+			this.liveClicker = features.liveClicker;
 			this.flashcard = features.flashcard;
 			this.total = features.total;
 			this.lecture = features.lecture;
@@ -158,6 +160,14 @@ public class SessionFeature {
 
 	public void setLiveFeedback(boolean liveFeedback) {
 		this.liveFeedback = liveFeedback;
+	}
+
+	public boolean isLiveClicker() {
+		return liveClicker;
+	}
+
+	public void setLiveClicker(boolean liveClicker) {
+		this.liveClicker = liveClicker;
 	}
 
 }

commit 9f7e2204eb59bd030941100df5c7371149cda92c

Fix NPE caused by old sessions without 'features' property
Fixes a regression introduced with commit
7207bad9d74ef23af14012a2440cb6ae5a394af5.

Fixes arsnova/arsnova#84

src/main/java/de/thm/arsnova/FeedbackStorage.java
@@ -118,7 +118,7 @@ public class FeedbackStorage {
 	public Map<Session, List<User>> cleanFeedbackVotes(final int cleanupFeedbackDelay) {
 		final Map<Session, List<User>> removedFeedbackOfUsersInSession = new HashMap<>();
 		for (final Session session : data.keySet()) {
-			if (!session.getFeatures().isLiveClicker()) {
+			if (session.getFeatures() == null || !session.getFeatures().isLiveClicker()) {
 				List<User> affectedUsers = cleanFeedbackVotesInSession(session, cleanupFeedbackDelay);
 				if (!affectedUsers.isEmpty()) {
 					removedFeedbackOfUsersInSession.put(session, affectedUsers);