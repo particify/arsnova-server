5f766c980690f572e3c3ba93194b20bb49119968
Daniel Gerhardt <code@dgerhardt.net>
2018-11-05 15:46:30 +0100

    Implement ResolvableTypeProvider to circumvent type erasure

--- src/main/java/de/thm/arsnova/event/CrudEvent.java
+++ src/main/java/de/thm/arsnova/event/CrudEvent.java
@@ -2,8 +2,10 @@ package de.thm.arsnova.event;
 
 import de.thm.arsnova.model.Entity;
 import org.springframework.context.ApplicationEvent;
+import org.springframework.core.ResolvableType;
+import org.springframework.core.ResolvableTypeProvider;
 
-public abstract class CrudEvent<E extends Entity> extends ApplicationEvent {
+public abstract class CrudEvent<E extends Entity> extends ApplicationEvent implements ResolvableTypeProvider {
 	public CrudEvent(final E source) {
 		super(source);
 	}
@@ -11,5 +13,10 @@ public abstract class CrudEvent<E extends Entity> extends ApplicationEvent {
 	@Override
 	public E getSource() {
 		return (E) super.getSource();
+	}
+
+	@Override
+	public ResolvableType getResolvableType() {
+		return ResolvableType.forClassWithGenerics(getClass(), source.getClass());
 	}
 }
