94b9df741ba96e895b917af3230d40b9d9ab6384
Tom Käsler <tom.kaesler@mni.thm.de>
2019-03-17 11:41:43 +0100

    Setup STOMP to optionaly use rabbit mq broker
    add queue naming conventions to doc
    
    add management interface into to rabbit file

--- src/main/java/de/thm/arsnova/config/WebSocketConfig.java
+++ src/main/java/de/thm/arsnova/config/WebSocketConfig.java
@@ -13,6 +13,16 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+	private final String MESSAGING_PREFIX = "/backend";
+	private final String[] DESTINATION_PREFIX = {"/exchange", "/topic", "/queue"};
+	private final String USER_REGISTRY_BROADCAST = "/topic/log-user-registry";
+	private final String USER_DESTINATION_BROADCAST = "/queue/log-unresolved-user";
+
+	@Value("${messaging.relay.enabled}") private Boolean relayEnabled;
+	@Value("${messaging.relay.host}") private String relayHost;
+	@Value("${messaging.relay.port}") private int relayPort;
+	@Value("${messaging.relay.user}") private String relayUser;
+	@Value("${messaging.relay.password}") private String relayPassword;
 
 	private final AuthChannelInterceptorAdapter authChannelInterceptorAdapter;
 	@Value(value = "${security.cors.origins:}") private String[] corsOrigins;
@@ -26,9 +36,20 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
-		config
-				.setApplicationDestinationPrefixes("/backend")
-				.enableSimpleBroker("/exchange", "/topic", "/queue");
+		config.setApplicationDestinationPrefixes(MESSAGING_PREFIX);
+
+		if (relayEnabled) {
+			config
+					.enableStompBrokerRelay(DESTINATION_PREFIX)
+					.setUserRegistryBroadcast(USER_REGISTRY_BROADCAST)
+					.setUserDestinationBroadcast(USER_DESTINATION_BROADCAST)
+					.setRelayHost(relayHost)
+					.setRelayPort(relayPort)
+					.setClientLogin(relayUser)
+					.setClientPasscode(relayPassword);
+		} else {
+			config.enableSimpleBroker(DESTINATION_PREFIX);
+		}
 	}
 
 	@Override
