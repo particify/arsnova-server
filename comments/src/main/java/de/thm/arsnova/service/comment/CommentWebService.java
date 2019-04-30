439bc6b3fe505357747f4d9a1c0e59bd4303776f
Tom Käsler <tom.kaesler@mni.thm.de>
2019-04-30 16:43:56 +0200

    Add war packaging

--- comments/src/main/java/de/thm/arsnova/service/comment/CommentWebService.java
+++ comments/src/main/java/de/thm/arsnova/service/comment/CommentWebService.java
@@ -2,6 +2,8 @@ package de.thm.arsnova.service.comment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
@@ -10,8 +12,14 @@ import org.springframework.context.annotation.PropertySource;
         ignoreResourceNotFound = true,
         encoding = "UTF-8"
 )
-public class CommentWebService {
-  public static void main(String[] args) {
-    SpringApplication.run(CommentWebService.class, args);
-  }
+public class CommentWebService extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CommentWebService.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommentWebService.class, args);
+    }
 }
