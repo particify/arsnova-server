dbaef881ea1a5a798bf3d0adb773a937a3f47d81
Tom Käsler <tom.kaesler@mni.thm.de>
2019-04-30 15:54:20 +0200

    Fix imports
    remove unneccessary imports
    use correct Nullable from checkerframework

--- src/main/java/de/thm/arsnova/config/WebSocketConfig.java
+++ src/main/java/de/thm/arsnova/config/WebSocketConfig.java
@@ -1,6 +1,10 @@
 package de.thm.arsnova.config;
 
+import de.thm.arsnova.websocket.handler.AuthChannelInterceptorAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -9,6 +13,16 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+	private final AuthChannelInterceptorAdapter authChannelInterceptorAdapter;
+	@Value(value = "${security.cors.origins:}") private String[] corsOrigins;
+
+	@Autowired
+	public WebSocketConfig(AuthChannelInterceptorAdapter authChannelInterceptorAdapter) {
+		this.authChannelInterceptorAdapter = authChannelInterceptorAdapter;
+	}
+
+
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -19,7 +33,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+		registry.addEndpoint("/ws").setAllowedOrigins(corsOrigins).withSockJS();
+	}
+
+
+	@Override
+	public void configureClientInboundChannel(ChannelRegistration registration) {
+		registration.setInterceptors(authChannelInterceptorAdapter);
 	}
 
 }
---
+++ src/main/java/de/thm/arsnova/websocket/handler/AuthChannelInterceptorAdapter.java
@@ -0,0 +1,65 @@
+package de.thm.arsnova.websocket.handler;
+
+import de.thm.arsnova.security.User;
+import de.thm.arsnova.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Component;
+
+import org.checkerframework.checker.nullness.qual.Nullable;
+import java.util.List;
+
+@Component
+public class AuthChannelInterceptorAdapter implements ChannelInterceptor {
+
+	private static final Logger logger = LoggerFactory.getLogger(AuthChannelInterceptorAdapter.class);
+
+	private final UserService service;
+
+	@Autowired
+	public AuthChannelInterceptorAdapter(final UserService service) {
+		this.service = service;
+	}
+
+	@Nullable
+	@Override
+	public Message<?> preSend(final Message<?> message, final MessageChannel channel) {
+		StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+
+		String sessionId = accessor.getSessionId();
+		if (accessor.getCommand() != null && accessor.getCommand().equals(StompCommand.CONNECT)) {
+			// user needs to authorize
+			List<String> tokenList = accessor.getNativeHeader("token");
+			if (tokenList != null && tokenList.size() > 0) {
+				String token = tokenList.get(0);
+				service.addWsSessionToJwtMapping(sessionId, token);
+			} else {
+				// no token given -> auth failed
+				logger.debug("no auth token given, dropping connection attempt");
+				return null;
+			}
+		} else {
+			List<String> userIdList = accessor.getNativeHeader("ars-user-id");
+			if (userIdList != null && userIdList.size() > 0) {
+				// user-id is given, check for auth
+				String userId = userIdList.get(0);
+				User u = service.getAuthenticatedUserByWsSession(sessionId);
+				if (u == null || !userId.equals(u.getId())) {
+					// user isn't authorized, drop message
+					logger.debug("user-id not validated, dropping frame");
+					return null;
+				}
+			}
+		}
+
+		// default is to pass the frame along
+		return MessageBuilder.createMessage(message.getPayload(), accessor.getMessageHeaders());
+	}
+}
