66f1e0bfd1087455a3fa1a6aeae658cd5eb146f5
Daniel Gerhardt <code@dgerhardt.net>
2019-06-04 11:15:18 +0200

    Add moderators for rooms
    Moderators can assist a room owner in managing the room. This commit
    adds API and persistence support for moderators.

--- src/main/java/de/thm/arsnova/controller/RoomController.java
+++ src/main/java/de/thm/arsnova/controller/RoomController.java
@@ -19,13 +19,22 @@ package de.thm.arsnova.controller;
 
 import de.thm.arsnova.model.Room;
 import de.thm.arsnova.service.RoomService;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
 
 @RestController
 @RequestMapping(RoomController.REQUEST_MAPPING)
 public class RoomController extends AbstractEntityController<Room> {
 	protected static final String REQUEST_MAPPING = "/room";
+	private static final String GET_MODERATORS_MAPPING = DEFAULT_ID_MAPPING + "/moderator";
+	private static final String MODERATOR_MAPPING = DEFAULT_ID_MAPPING + "/moderator/{userId}";
 
 	private RoomService roomService;
 
@@ -42,5 +51,30 @@ public class RoomController extends AbstractEntityController<Room> {
 	@Override
 	protected String resolveAlias(final String shortId) {
 		return roomService.getIdByShortId(shortId);
+	}
+
+	@GetMapping(GET_MODERATORS_MAPPING)
+	public Set<Room.Moderator> getModerators(@PathVariable final String id) {
+		return roomService.get(id).getModerators();
+	}
+
+	@PutMapping(MODERATOR_MAPPING)
+	public void putModerator(@PathVariable final String id, @PathVariable final String userId,
+			@RequestBody final Room.Moderator moderator) {
+		final Room room = roomService.get(id);
+		moderator.setUserId(userId);
+		if (moderator.getRoles().isEmpty()) {
+			moderator.getRoles().add(Room.Moderator.Role.EXECUTIVE_MODERATOR);
+		}
+		room.getModerators().removeIf(m -> m.getUserId().equals(userId));
+		room.getModerators().add(moderator);
+		roomService.update(room);
+	}
+
+	@DeleteMapping(MODERATOR_MAPPING)
+	public void deleteModerator(@PathVariable final String id, @PathVariable final String userId) {
+		final Room room = roomService.get(id);
+		room.getModerators().removeIf(m -> m.getUserId().equals(userId));
+		roomService.update(room);
 	}
 }
