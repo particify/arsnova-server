f8a36b46e4bcae1a67cd2273efdde2e45a0fde53
Tom Käsler <tom.kaesler@mni.thm.de>
2019-06-10 01:24:41 +0200

    Add room access model for authorization purpose

---
+++ comments/src/main/java/de/thm/arsnova/service/comment/service/RoomAccessService.java
@@ -0,0 +1,30 @@
+package de.thm.arsnova.service.comment.service;
+
+import de.thm.arsnova.service.comment.model.RoomAccess;
+import de.thm.arsnova.service.comment.model.RoomAccessPK;
+import de.thm.arsnova.service.comment.service.persistence.RoomAccessRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoomAccessService {
+    private static final Logger logger = LoggerFactory.getLogger(RoomAccessService.class);
+
+    final RoomAccessRepository repository;
+
+    @Autowired
+    public RoomAccessService(RoomAccessRepository repository) {
+        this.repository = repository;
+    }
+
+    public RoomAccess get(RoomAccessPK pk) {
+        return repository.findById(pk).orElse(new RoomAccess());
+    }
+
+    public RoomAccess create(RoomAccess roomAccess) {
+        logger.trace("Creating RoomAccess: " + roomAccess.toString());
+        repository.save(roomAccess);
+    }
+}
