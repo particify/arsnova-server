df44c81a2eb41212104290be354c16f453fe3eca
Tom Käsler <tom.kaesler@mni.thm.de>
2019-07-26 02:21:16 +0200

    Add settings for room
    Add a directSend flag to indicate whether the comments need a "go" from a moderator.

---
+++ comments/src/main/java/de/thm/arsnova/service/comment/handler/SettingsCommandHandler.java
@@ -0,0 +1,52 @@
+package de.thm.arsnova.service.comment.handler;
+
+import de.thm.arsnova.service.comment.model.Settings;
+import de.thm.arsnova.service.comment.model.command.CreateSettings;
+import de.thm.arsnova.service.comment.model.command.CreateSettingsPayload;
+import de.thm.arsnova.service.comment.model.command.UpdateSettings;
+import de.thm.arsnova.service.comment.model.command.UpdateSettingsPayload;
+import de.thm.arsnova.service.comment.service.SettingsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SettingsCommandHandler {
+    private static final Logger logger = LoggerFactory.getLogger(SettingsCommandHandler.class);
+
+    private final SettingsService service;
+
+    @Autowired
+    public SettingsCommandHandler(SettingsService service) {
+        this.service = service;
+    }
+
+    public Settings handle(CreateSettings command) {
+        logger.trace("got new command: " + command.toString());
+
+        CreateSettingsPayload payload = command.getPayload();
+
+        Settings newSettings = new Settings();
+        newSettings.setRoomId(payload.getRoomId());
+        newSettings.setDirectSend(payload.getDirectSend());
+
+        Settings saved = service.create(newSettings);
+
+        return saved;
+    }
+
+    public Settings handle(UpdateSettings command) {
+        logger.trace("got new command: " + command.toString());
+
+        UpdateSettingsPayload payload = command.getPayload();
+        Settings settings = service.get(payload.getRoomId());
+        settings.setRoomId(payload.getRoomId());
+        settings.setDirectSend(payload.getDirectSend());
+
+        Settings updated = service.update(settings);
+
+        return updated;
+
+    }
+}
---
+++ comments/src/main/java/de/thm/arsnova/service/comment/service/SettingsService.java
@@ -0,0 +1,37 @@
+package de.thm.arsnova.service.comment.service;
+
+import de.thm.arsnova.service.comment.model.Settings;
+import de.thm.arsnova.service.comment.service.persistence.SettingsRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SettingsService {
+    private static final Logger logger = LoggerFactory.getLogger(SettingsService.class);
+    private final SettingsRepository repository;
+
+    @Autowired
+    public SettingsService(
+            final SettingsRepository repository
+    ) {
+        this.repository = repository;
+    }
+
+    public Settings get(String id) {
+        Settings s = repository.findById(id).orElse(new Settings());
+
+        return s;
+    }
+
+    public Settings create(Settings s) {
+        repository.save(s);
+
+        return s;
+    }
+
+    public Settings update(final Settings s) {
+        return repository.save(s);
+    }
+}
