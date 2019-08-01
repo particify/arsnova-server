d436194d82cfc904431a8adaef1011a97f2c9aaa
Daniel Gerhardt <code@dgerhardt.net>
2019-08-01 17:02:20 +0200

    Remove Dropwizard/Stagemonitor metrics related code and deps
    Methods annotated with @Timed now use Micrometers annotation. The @Gauge
    annotation has been remove without replacement for now.
    
    While configuration for the use of Micrometer's TimedAspect has been
    prepared, it is not yet active because of compatibility issues with
    compile-time weaving.
    See https://github.com/micrometer-metrics/micrometer/issues/1149.

--- src/main/java/de/thm/arsnova/config/AppConfig.java
+++ src/main/java/de/thm/arsnova/config/AppConfig.java
@@ -37,7 +37,12 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.AdviceMode;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
