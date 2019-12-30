752235da72dae66f90fdcc65b305fbd9f8fb8bd6
Daniel Gerhardt <code@dgerhardt.net>
2019-12-30 19:24:20 +0100

    Add separate configs for environments

---
+++ .docker/compose/production/set-env.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+ENV=$1
+ENV_FILE=docker-compose.$ENV.yml
+if [ ! -f "$ENV_FILE" ]; then
+  echo No configuration for environment \"$ENV\" found.
+  exit 1
+fi
+ln -sf docker-compose.$ENV.yml docker-compose.override.yml
