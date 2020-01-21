526df702b38dfbebc0c2cb60a828cd5da419a5d7
Daniel Gerhardt <code@dgerhardt.net>
2020-01-22 00:29:47 +0100

    Allow configuration of event message publisher
    Configuration properties have been added to adjust RabbitMQ connection
    settings and define events for publishing.
    
    Example for event config:
    
        arsnova:
          system:
            message-broker:
              rabbitmq:
                enabled: true
              published-events:
                - entity-type: Room
                  event-type: AfterCreation
                  included-properties:
                    - id
                    - shortId
                    - ownerId

--- src/main/java/de/thm/arsnova/config/RabbitConfig.java
+++ src/main/java/de/thm/arsnova/config/RabbitConfig.java
@@ -1,8 +1,8 @@
 package de.thm.arsnova.config;
 
-import java.util.Arrays;
-import java.util.List;
-import org.springframework.amqp.core.Declarable;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.springframework.amqp.core.Declarables;
 import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
@@ -11,60 +11,62 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
 
+import de.thm.arsnova.config.properties.MessageBrokerProperties;
+import de.thm.arsnova.event.AmqpEventDispatcher;
+
 @Configuration
 public class RabbitConfig {
-	private static final String roomAfterCreationEventExchangeName = "backend.room.aftercreationevent";
-	private static final String roomAfterDeletionEventExchangeName = "backend.room.afterdeletionevent";
-	private static final String roomAfterFullUpdateEventExchangeName = "backend.room.afterfullupdateevent";
-	private static final String roomAfterPatchEventExchangeName = "backend.room.afterpatchevent";
-	private static final String roomAfterUpdateEventExchangeName = "backend.room.afterupdateevent";
+	private static final String RABBIT_ENABLED = "rabbitmq.enabled";
+	private static final String RABBIT_MANAGE_DECLARATIONS = "rabbitmq.manage-declarations";
 
 	@Bean
 	@Autowired
+	@ConditionalOnProperty(
+			name = RABBIT_ENABLED,
+			prefix = MessageBrokerProperties.PREFIX,
+			havingValue = "true")
 	public ConnectionFactory connectionFactory(
-			@TaskExecutorConfig.RabbitConnectionExecutor final TaskExecutor executor
+			@TaskExecutorConfig.RabbitConnectionExecutor final TaskExecutor executor,
+			final MessageBrokerProperties messageBrokerProperties
 	) {
-		final CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost", 5672);
-		connectionFactory.setUsername("guest");
-		connectionFactory.setPassword("guest");
+		final CachingConnectionFactory connectionFactory = new CachingConnectionFactory(
+				messageBrokerProperties.getRabbitmq().getHost(),
+				messageBrokerProperties.getRabbitmq().getPort());
+		connectionFactory.setUsername(messageBrokerProperties.getRabbitmq().getUsername());
+		connectionFactory.setPassword(messageBrokerProperties.getRabbitmq().getPassword());
+		connectionFactory.setVirtualHost(messageBrokerProperties.getRabbitmq().getVirtualHost());
 		connectionFactory.setExecutor(executor);
-		connectionFactory.setVirtualHost("/");
 
 		return connectionFactory;
 	}
 
 	@Bean
 	@Autowired
-	public List<Declarable> declareExchanges(final RabbitAdmin rabbitAdmin) {
-		final FanoutExchange roomAfterCreationEventExchange = new FanoutExchange(roomAfterCreationEventExchangeName);
-		final FanoutExchange roomAfterDeletionEventExchange = new FanoutExchange(roomAfterDeletionEventExchangeName);
-		final FanoutExchange roomAfterFullUpdateEventExchange = new FanoutExchange(roomAfterFullUpdateEventExchangeName);
-		final FanoutExchange roomAfterPatchEventExchange = new FanoutExchange(roomAfterPatchEventExchangeName);
-		final FanoutExchange roomAfterUpdateEventExchange = new FanoutExchange(roomAfterUpdateEventExchangeName);
-
-		rabbitAdmin.declareExchange(roomAfterCreationEventExchange);
-		rabbitAdmin.declareExchange(roomAfterDeletionEventExchange);
-		rabbitAdmin.declareExchange(roomAfterFullUpdateEventExchange);
-		rabbitAdmin.declareExchange(roomAfterPatchEventExchange);
-		rabbitAdmin.declareExchange(roomAfterUpdateEventExchange);
-
-		return Arrays.asList(
-				roomAfterCreationEventExchange,
-				roomAfterDeletionEventExchange,
-				roomAfterFullUpdateEventExchange,
-				roomAfterPatchEventExchange,
-				roomAfterUpdateEventExchange
-		);
+	@ConditionalOnProperty(
+			name = {RABBIT_ENABLED, RABBIT_MANAGE_DECLARATIONS},
+			prefix = MessageBrokerProperties.PREFIX,
+			havingValue = "true")
+	public Declarables declarables(
+			final MessageBrokerProperties messageBrokerProperties) {
+		final Set<String> eventExchanges = messageBrokerProperties.getPublishedEvents().stream()
+				.map(c -> AmqpEventDispatcher.makeQueueName(c.entityType, c.eventType)).collect(Collectors.toSet());
+		return new Declarables(eventExchanges.stream()
+				.map(FanoutExchange::new).collect(Collectors.toList()));
 	}
 
 	@Bean
 	@Autowired
+	@ConditionalOnProperty(
+			name = RABBIT_ENABLED,
+			prefix = MessageBrokerProperties.PREFIX,
+			havingValue = "true")
 	public RabbitTemplate rabbitTemplate(
 			final ConnectionFactory connectionFactory,
 			final MessageConverter messageConverter) {
@@ -75,22 +77,49 @@ public class RabbitConfig {
 
 	@Bean
 	@Autowired
+	@ConditionalOnProperty(
+			name = RABBIT_ENABLED,
+			prefix = MessageBrokerProperties.PREFIX,
+			havingValue = "true")
 	public RabbitAdmin rabbitAdmin(final ConnectionFactory connectionFactory) {
 		return new RabbitAdmin(connectionFactory);
 	}
 
 	@Bean
+	@ConditionalOnProperty(
+			name = RABBIT_ENABLED,
+			prefix = MessageBrokerProperties.PREFIX,
+			havingValue = "true")
+	public AmqpEventDispatcher eventToTopicPublisher(
+			final RabbitTemplate rabbitTemplate,
+			final MessageBrokerProperties messageBrokerProperties) {
+		return new AmqpEventDispatcher(rabbitTemplate, messageBrokerProperties);
+	}
+
+	@Bean
+	@ConditionalOnProperty(
+			name = RABBIT_ENABLED,
+			prefix = MessageBrokerProperties.PREFIX,
+			havingValue = "true")
 	public MessageConverter jsonMessageConverter() {
 		return new Jackson2JsonMessageConverter();
 	}
 
 	@Bean
+	@ConditionalOnProperty(
+			name = RABBIT_ENABLED,
+			prefix = MessageBrokerProperties.PREFIX,
+			havingValue = "true")
 	public MappingJackson2MessageConverter mappingJackson2MessageConverter() {
 		final MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
 		return converter;
 	}
 
 	@Bean
+	@ConditionalOnProperty(
+			name = RABBIT_ENABLED,
+			prefix = MessageBrokerProperties.PREFIX,
+			havingValue = "true")
 	public DefaultMessageHandlerMethodFactory defaultMessageHandlerMethodFactory() {
 		final DefaultMessageHandlerMethodFactory factory = new DefaultMessageHandlerMethodFactory();
 		factory.setMessageConverter(mappingJackson2MessageConverter());
--- src/main/java/de/thm/arsnova/event/AmqpEventDispatcher.java
+++ src/main/java/de/thm/arsnova/event/AmqpEventDispatcher.java
@@ -5,78 +5,77 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
+import java.util.Collections;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Component;
 
+import de.thm.arsnova.config.properties.MessageBrokerProperties;
 import de.thm.arsnova.model.Entity;
 
 /**
  * AmqpEventDispatcher listens to application events, converts them to messages and sends them to RabbitMQ.
+ * Events can be filtered via configuration to those needed by other services.
  * Use the 'backend.' prefix for internal events that should not be sent to frontend clients.
- * Restrict messages to what is needed in other services as long as the arsnova-backend is owner of the functionality.
+ *
+ * @author Tom Käsler
+ * @author Daniel Gerhardt
  */
-@Component
 public class AmqpEventDispatcher {
 	@JsonFilter("amqpPropertyFilter")
 	public static class AmqpPropertyFilter {
 	}
 
-	private static class MessagePublishingConfig {
-		public String entityType;
-		public String applicationEventType;
-		public Set<String> attributes = new HashSet<>();
-	}
-
+	private static final String PREFIX = "backend.event.";
 	private static final Logger logger = LoggerFactory.getLogger(AmqpEventDispatcher.class);
 
 	private final RabbitTemplate messagingTemplate;
-
-	private List<MessagePublishingConfig> config = new ArrayList<>();
+	private final Map<String, Set> eventConfig;
 
 	@Autowired
-	public AmqpEventDispatcher(final RabbitTemplate rabbitTemplate) {
+	public AmqpEventDispatcher(
+			final RabbitTemplate rabbitTemplate,
+			final MessageBrokerProperties messageBrokerProperties) {
 		messagingTemplate = rabbitTemplate;
-		final MessagePublishingConfig config = new MessagePublishingConfig();
-		config.entityType = "Room";
-		config.applicationEventType = "AfterCreationEvent";
-		config.attributes.add("id");
-		config.attributes.add("ownerId");
-		this.config.add(config);
+		eventConfig = messageBrokerProperties.getPublishedEvents().stream().collect(Collectors.toMap(
+				c -> c.entityType + "-" + c.eventType,
+				c -> c.includedProperties));
+	}
+
+	public static String makeQueueName(final String entityType, final String eventType) {
+		return PREFIX + entityType.toLowerCase() + "." + eventType.toLowerCase();
 	}
 
 	@EventListener
 	public <T extends CrudEvent, E extends Entity> void dispatchEntityCrudEvent(final T event) {
 		logger.trace("Dispatching event ({}) for AMQP.", event.getClass().getSimpleName());
-		final String eventType = event.getClass().getSimpleName();
+		String eventType = event.getClass().getSimpleName();
+		eventType = eventType.substring(0, eventType.length() - 5);
 		final String entityType = event.getEntity().getClass().getSimpleName();
+		final String key = entityType + "-" + eventType;
+		final Set<String> properties = eventConfig.getOrDefault(key, Collections.emptySet());
 
-		for (final MessagePublishingConfig config : this.config) {
-			if (config.applicationEventType.equals(eventType)) {
-				final String exchangeName = "backend." + entityType.toLowerCase() + "." + eventType.toLowerCase();
-				final ObjectMapper mapper = new ObjectMapper();
-				final SimpleFilterProvider filterProvider = new SimpleFilterProvider();
-				filterProvider.addFilter("amqpPropertyFilter",
-						SimpleBeanPropertyFilter.filterOutAllExcept(config.attributes));
-				mapper.setFilterProvider(filterProvider);
-				mapper.addMixIn(Entity.class, AmqpPropertyFilter.class);
-				try {
-					final String jsonPayload = mapper.writeValueAsString(event.getEntity());
-					logger.debug("AMQP event payload: {}", jsonPayload);
-					messagingTemplate.convertAndSend(exchangeName, "", jsonPayload);
-				} catch (final JsonProcessingException e) {
-					logger.error("Event serialization failed.", e);
-				} catch (final AmqpException e) {
-					logger.error("Could not send event to broker.", e);
-				}
+		if (!properties.isEmpty()) {
+			final String exchangeName = makeQueueName(entityType, eventType);
+			final ObjectMapper mapper = new ObjectMapper();
+			final SimpleFilterProvider filterProvider = new SimpleFilterProvider();
+			filterProvider.addFilter("amqpPropertyFilter", SimpleBeanPropertyFilter.filterOutAllExcept(properties));
+			mapper.setFilterProvider(filterProvider);
+			mapper.addMixIn(Entity.class, AmqpPropertyFilter.class);
+			try {
+				final String jsonPayload = mapper.writeValueAsString(event.getEntity());
+				logger.debug("AMQP event payload: {}", jsonPayload);
+				messagingTemplate.convertAndSend(exchangeName, "", jsonPayload);
+			} catch (final JsonProcessingException e) {
+				logger.error("Event serialization failed.", e);
+			} catch (final AmqpException e) {
+				logger.error("Could not send event to broker.", e);
 			}
 		}
 	}
