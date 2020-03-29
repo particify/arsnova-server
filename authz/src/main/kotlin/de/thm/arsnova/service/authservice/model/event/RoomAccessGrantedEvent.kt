6ce7951d86a8c5dad46329a53a4f9dab1408db8c
tekay <tom.kaesler@mni.thm.de>
2020-03-29 15:28:17 +0200

    Add rev to room access

--- authz/src/main/kotlin/de/thm/arsnova/service/authservice/model/event/RoomAccessGrantedEvent.kt
+++ authz/src/main/kotlin/de/thm/arsnova/service/authservice/model/event/RoomAccessGrantedEvent.kt
@@ -2,6 +2,7 @@ package de.thm.arsnova.service.authservice.model.event
 
 data class RoomAccessGrantedEvent (
         val version: String = "",
+        val rev: String = "",
         val roomId: String = "",
         val userId: String = "",
         val role: String = ""
