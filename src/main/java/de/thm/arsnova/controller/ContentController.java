ab888ffafacf8df2fd2867bb61881a0f7c8afcda
tekay <tom.kaesler@mni.thm.de>
2020-05-16 09:50:27 +0200

    Add bulk delete route for all answers to a content

--- src/main/java/de/thm/arsnova/controller/ContentController.java
+++ src/main/java/de/thm/arsnova/controller/ContentController.java
@@ -18,6 +18,7 @@
 
 package de.thm.arsnova.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,6 +34,7 @@ import de.thm.arsnova.service.ContentService;
 public class ContentController extends AbstractEntityController<Content> {
 	protected static final String REQUEST_MAPPING = "/content";
 	private static final String GET_ANSWER_STATISTICS_MAPPING = DEFAULT_ID_MAPPING + "/stats";
+	private static final String DELETE_ANSWERS_MAPPING = DEFAULT_ID_MAPPING + "/answer";
 
 	private ContentService contentService;
 	private AnswerService answerService;
@@ -51,5 +53,10 @@ public class ContentController extends AbstractEntityController<Content> {
 	@GetMapping(GET_ANSWER_STATISTICS_MAPPING)
 	public AnswerStatistics getAnswerStatistics(@PathVariable final String id) {
 		return answerService.getAllStatistics(id);
+	}
+
+	@DeleteMapping(DELETE_ANSWERS_MAPPING)
+	public void deleteAnswers(@PathVariable final String id) {
+		answerService.deleteAnswers(id);
 	}
 }
