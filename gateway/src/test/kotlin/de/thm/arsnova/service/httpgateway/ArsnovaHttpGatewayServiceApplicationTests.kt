5b951b916002a82ba9647ea780bd155fc67d8e02
tekay <tk@particify.de>
2020-06-18 13:27:13 +0200

    INIT commit

---
+++ gateway/src/test/kotlin/de/thm/arsnova/service/httpgateway/ArsnovaHttpGatewayServiceApplicationTests.kt
@@ -0,0 +1,12 @@
+package de.thm.arsnova.service.httpgateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArsnovaHttpGatewayServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+}
