ca46809d61a8cdfce4465aad7c2e4913e9567ff0
tekay <tom.kaesler@mni.thm.de>
2020-08-07 01:29:28 +0200

    Add ack comment count to room summary

--- gateway/src/main/kotlin/de/thm/arsnova/service/httpgateway/view/RoomView.kt
+++ gateway/src/main/kotlin/de/thm/arsnova/service/httpgateway/view/RoomView.kt
@@ -6,6 +6,7 @@ import de.thm.arsnova.service.httpgateway.model.RoomStats
 import de.thm.arsnova.service.httpgateway.model.RoomSummary
 import de.thm.arsnova.service.httpgateway.service.CommentService
 import de.thm.arsnova.service.httpgateway.service.RoomService
+import org.springframework.security.core.context.ReactiveSecurityContextHolder
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Flux
 
@@ -15,21 +16,25 @@ class RoomView(
     private val commentService: CommentService
 ) {
     fun getSummaries(roomIds: List<String>): Flux<RoomSummary> {
-        val rooms: Flux<Room> = roomService.get(roomIds)
-        val commentStats: Flux<CommentStats> = commentService.getStats(roomIds)
-
-        return Flux
-            .zip(rooms, commentStats)
-            .map { tuple2 ->
-                RoomSummary(
-                    tuple2.t1.id,
-                    tuple2.t1.shortId,
-                    tuple2.t1.name,
-                    RoomStats(
-                        0,
-                        tuple2.t2.ackCommentCount
+        return ReactiveSecurityContextHolder.getContext()
+                .map { securityContext ->
+                    securityContext.authentication.principal
+                }
+                .cast(String::class.java)
+                .flatMapMany { jwt: String ->
+                    commentService.getStats(roomIds, jwt)
+                }
+                .zipWith(roomService.get(roomIds))
+                .map { tuple2 ->
+                    RoomSummary(
+                            tuple2.t2.id,
+                            tuple2.t2.shortId,
+                            tuple2.t2.name,
+                            RoomStats(
+                                    0,
+                                    tuple2.t1.ackCommentCount
+                            )
                     )
-                )
-            }
+                }
     }
 }
