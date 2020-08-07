034088817de56fd883a7af5d73b689e7ec3e2b48
tekay <tom.kaesler@mni.thm.de>
2020-08-07 02:01:47 +0200

    Add content service to query content counter for rooms

--- gateway/src/main/kotlin/de/thm/arsnova/service/httpgateway/service/CommentService.kt
+++ gateway/src/main/kotlin/de/thm/arsnova/service/httpgateway/service/CommentService.kt
@@ -17,7 +17,7 @@ class CommentService(
 
     fun getStats(roomIds: List<String>, jwt: String): Flux<CommentStats> {
         val url = "${httpGatewayProperties.httpClient!!.commentService}/comment/stats?roomIds=${roomIds.joinToString(",")}"
-        logger.trace("Querying comment service for stats with url: {}", url)
+        logger.trace("Querying comment service for comment stats with url: {}", url)
         return webClient.get()
                 .uri(url)
                 .header("Authorization", jwt)
