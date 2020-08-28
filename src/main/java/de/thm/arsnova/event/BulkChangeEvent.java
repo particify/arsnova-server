7123cb4bfc5c81a802de03771c21cd7cd05e7ecc
Daniel Gerhardt <code@dgerhardt.net>
2020-08-28 18:26:29 +0200

    Publish a new BulkChangeEvent
    The `BulkChangeEvent` is published after multiple entities have been
    patched or deleted. It can (and should) also be used after bulk creation
    but there currently is no generalized bulk creation method for entities,
    so publishing needs to be handled explicitly by the respective entity
    service. This is the case for the AnswerService's scheduled bulk
    creation.

---
+++ src/main/java/de/thm/arsnova/event/BulkChangeEvent.java
@@ -0,0 +1,49 @@
+/*
+ * This file is part of ARSnova Backend.
+ * Copyright (C) 2012-2019 The ARSnova Team and Contributors
+ *
+ * ARSnova Backend is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ARSnova Backend is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.thm.arsnova.event;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.core.ResolvableType;
+import org.springframework.core.ResolvableTypeProvider;
+
+import de.thm.arsnova.model.Entity;
+
+public class BulkChangeEvent<E extends Entity> extends ApplicationEvent implements ResolvableTypeProvider {
+	private Iterable<E> entities;
+	private Class<E> clazz;
+
+	public BulkChangeEvent(final Object source, Class<E> clazz, final Iterable<E> entities) {
+		super(source);
+		this.clazz = clazz;
+		this.entities = entities;
+	}
+
+	public Iterable<E> getEntities() {
+		return entities;
+	}
+
+	public void setEntities(final Iterable<E> entities) {
+		this.entities = entities;
+	}
+
+	@Override
+	public ResolvableType getResolvableType() {
+		return ResolvableType.forClassWithGenerics(getClass(), clazz);
+	}
+}
