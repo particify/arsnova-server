c83a5a22c4953f379ab886de8dcc1ca137b36a3a
tekay <tk@particify.de>
2020-09-14 12:07:59 +0200

    Add room auth filter to update room access at auth service
    Introduce a new filter that listens to changes in room access.
    Whenever a room is created, deleted, transfered, getting or losing a new moderator,
    a request to the auth service is chained after the core has handled it.
    This lets the core handle business logic first and only if that part of the requests succeeds,
    the auth service is informed about new access.
    
    This allows for completely removing room access handling in the core.

--- gateway/src/main/kotlin/de/thm/arsnova/service/httpgateway/service/RoomAccessService.kt
+++ gateway/src/main/kotlin/de/thm/arsnova/service/httpgateway/service/RoomAccessService.kt
@@ -4,6 +4,7 @@ import de.thm.arsnova.service.httpgateway.config.HttpGatewayProperties
 import de.thm.arsnova.service.httpgateway.model.RoomAccess
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
@@ -20,6 +21,31 @@ class RoomAccessService(
         logger.trace("Querying auth service for room access with url: {}", url)
         return webClient.get().uri(url)
                 .retrieve().bodyToMono(RoomAccess::class.java).cache()
+    }
+
+    fun postRoomAccess(roomAccess: RoomAccess): Mono<RoomAccess> {
+        val url = "${httpGatewayProperties.httpClient.authService}/roomaccess/"
+        logger.trace("Posting to auth service for room access with url: {}, roomAccess: {}", url, roomAccess)
+        return webClient.post().uri(url)
+            .body(BodyInserters.fromPublisher(Mono.just(roomAccess), RoomAccess::class.java))
+            .retrieve()
+            .bodyToMono(RoomAccess::class.java)
+    }
+
+    fun deleteRoomAccess(roomAccess: RoomAccess): Mono<RoomAccess> {
+        val url = "${httpGatewayProperties.httpClient.authService}/roomaccess/${roomAccess.roomId}/${roomAccess.userId}"
+        logger.trace("Deleting to auth service for room access with url: {}, roomAccess: {}", url, roomAccess)
+        return webClient.delete().uri(url)
+            .retrieve()
+            .bodyToMono(RoomAccess::class.java)
+    }
+
+    fun deleteRoomAccessByRoomId(roomId: String): Flux<RoomAccess> {
+        val url = "${httpGatewayProperties.httpClient.authService}/roomaccess/${roomId}"
+        logger.trace("Deleting to auth service for room access with url: {}", url)
+        return webClient.delete().uri(url)
+            .retrieve()
+            .bodyToFlux(RoomAccess::class.java)
     }
 
     fun getRoomAccessByUser(userId: String): Flux<RoomAccess> {
