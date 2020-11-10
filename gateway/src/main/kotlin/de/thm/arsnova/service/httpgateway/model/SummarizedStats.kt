7a579797b12aed29ea44fd354d03e196cea809d5
Daniel Gerhardt <code@dgerhardt.net>
2020-11-10 18:24:41 +0100

    Improve stat summary for user profiles
    * Use `accountCount` instead of `totalCount` as user count.
    * Add pending activations.

--- gateway/src/main/kotlin/de/thm/arsnova/service/httpgateway/model/SummarizedStats.kt
+++ gateway/src/main/kotlin/de/thm/arsnova/service/httpgateway/model/SummarizedStats.kt
@@ -3,6 +3,7 @@ package de.thm.arsnova.service.httpgateway.model
 data class SummarizedStats(
     val connectedUsers: Int,
     val users: Int,
+    val activationsPending: Int,
     val rooms: Int,
     val contents: Int,
     val answers: Int,
