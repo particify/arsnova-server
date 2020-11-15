216b33f274ebe2aca348181ea095ebf256275585
Daniel Gerhardt <code@dgerhardt.net>
2020-11-15 12:20:59 +0100

    Add auth provider fallback setting for migration
    The fallback is used if the `ÀuthProvider` of a `UserProfile` cannot be
    detected automatically (usually the case for self-managed SSO).

--- src/main/java/de/thm/arsnova/config/properties/CouchDbMigrationProperties.java
+++ src/main/java/de/thm/arsnova/config/properties/CouchDbMigrationProperties.java
@@ -2,11 +2,14 @@ package de.thm.arsnova.config.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import de.thm.arsnova.model.UserProfile;
+
 @ConfigurationProperties(CouchDbMigrationProperties.PREFIX)
 public class CouchDbMigrationProperties extends CouchDbProperties {
 	public static final String PREFIX = SystemProperties.PREFIX + ".couchdb.migration";
 
 	private boolean enabled;
+	private UserProfile.AuthProvider authenticationProviderFallback;
 
 	public boolean isEnabled() {
 		return enabled;
@@ -14,5 +17,13 @@ public class CouchDbMigrationProperties extends CouchDbProperties {
 
 	public void setEnabled(final boolean enabled) {
 		this.enabled = enabled;
+	}
+
+	public UserProfile.AuthProvider getAuthenticationProviderFallback() {
+		return authenticationProviderFallback;
+	}
+
+	public void setAuthenticationProviderFallback(final UserProfile.AuthProvider authenticationProviderFallback) {
+		this.authenticationProviderFallback = authenticationProviderFallback;
 	}
 }
