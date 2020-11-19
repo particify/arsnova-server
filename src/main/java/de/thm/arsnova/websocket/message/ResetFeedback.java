89f8e12a53e155a83144ffc37de10c6f1e5fc17c
tekay <tk@particify.de>
2020-11-19 17:16:31 +0100

    Fix giving correct class

--- src/main/java/de/thm/arsnova/websocket/message/ResetFeedback.java
+++ src/main/java/de/thm/arsnova/websocket/message/ResetFeedback.java
@@ -2,6 +2,6 @@ package de.thm.arsnova.websocket.message;
 
 public class ResetFeedback extends WebSocketMessage<ResetFeedbackPayload> {
 	public ResetFeedback() {
-		super(GetFeedback.class.getSimpleName());
+		super(ResetFeedback.class.getSimpleName());
 	}
 }
