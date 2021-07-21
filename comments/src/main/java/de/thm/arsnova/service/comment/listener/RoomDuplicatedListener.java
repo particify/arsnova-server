eef2a12f6b1662210744c881e8a372a86112b8bb
Daniel Gerhardt <code@dgerhardt.net>
2021-07-21 13:08:06 +0200

    Create a single vote with the sum for duplicated comments

--- comments/src/main/java/de/thm/arsnova/service/comment/listener/RoomDuplicatedListener.java
+++ comments/src/main/java/de/thm/arsnova/service/comment/listener/RoomDuplicatedListener.java
@@ -1,33 +1,66 @@
 package de.thm.arsnova.service.comment.listener;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import de.thm.arsnova.service.comment.config.RabbitConfig;
 import de.thm.arsnova.service.comment.model.Comment;
+import de.thm.arsnova.service.comment.model.Vote;
 import de.thm.arsnova.service.comment.service.CommentService;
+import de.thm.arsnova.service.comment.service.VoteService;
 
 @Service
 public class RoomDuplicatedListener {
     private static final String NIL_UUID = "00000000-0000-0000-0000-000000000000";
     private CommentService commentService;
+    private VoteService voteService;
 
-    public RoomDuplicatedListener(final CommentService commentService) {
+    public RoomDuplicatedListener(
+            final CommentService commentService,
+            final VoteService voteService) {
         this.commentService = commentService;
+        this.voteService = voteService;
     }
 
     @RabbitListener(queues = RabbitConfig.BACKEND_ROOM_DUPLICATED_QUEUE_NAME)
+    @Transactional
     public void receiveMessage(final RoomDuplicatedMessage message) {
-        final List<Comment> comments = commentService.getByRoomIdAndArchiveIdNull(message.originalRoomId);
+        Map<String, Comment> commentMapping = duplicateComments(message.originalRoomId, message.duplicatedRoomId);
+        duplicateVotes(message.originalRoomId, commentMapping);
+    }
+
+    private Map<String, Comment> duplicateComments(final String originalRoomId, final String duplicatedRoomId) {
+        final Map<String, Comment> commentMapping = new HashMap<>();
+        final List<Comment> comments = commentService.getByRoomIdAndArchiveIdNull(originalRoomId);
         final List<Comment> commentCopies = comments.stream().map(c -> {
             final Comment commentCopy = new Comment(c);
+            commentMapping.put(c.getId(), commentCopy);
             commentCopy.setCreatorId(NIL_UUID);
-            commentCopy.setRoomId(message.duplicatedRoomId);
+            commentCopy.setRoomId(duplicatedRoomId);
             return commentCopy;
         }).collect(Collectors.toList());
         commentService.create(commentCopies);
+
+        return commentMapping;
+    }
+
+    private void duplicateVotes(final String originalRoomId, Map<String, Comment> commentMapping) {
+        final Map<String, Integer> voteSums = voteService.getSumByCommentForRoom(originalRoomId);
+        for (final Map.Entry<String, Integer> voteSum : voteSums.entrySet()) {
+            if (!commentMapping.containsKey(voteSum.getKey())) {
+                continue;
+            }
+            final Vote vote = new Vote();
+            vote.setUserId(NIL_UUID);
+            vote.setCommentId(commentMapping.get(voteSum.getKey()).getId());
+            vote.setVote(voteSum.getValue());
+            voteService.create(vote);
+        }
     }
 
     private static class RoomDuplicatedMessage {
