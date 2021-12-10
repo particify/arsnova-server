3b0d6be000b0c18533033ee313e9ba9c0a74c317
Daniel Gerhardt <code@dgerhardt.net>
2021-12-10 16:01:01 +0100

    Resolve circular dependency
    Mark setter for JwtTokenFiler in SecurityConfig as `@Lazy` to allow
    Spring to break the cycle.
    
    Spring Boot error:
    
        The dependencies of some of the beans in the application context form a cycle:
    
        ┌─────┐
        |  jwtTokenFilter
        ↑     ↓
        |  securityConfig
        └─────┘

--- comments/src/main/java/de/thm/arsnova/service/comment/config/SecurityConfig.java
+++ comments/src/main/java/de/thm/arsnova/service/comment/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointPr
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -50,6 +51,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Autowired
+    @Lazy
     public void setJwtTokenFilter(final JwtTokenFilter jwtTokenFilter) {
         this.jwtTokenFilter = jwtTokenFilter;
     }
