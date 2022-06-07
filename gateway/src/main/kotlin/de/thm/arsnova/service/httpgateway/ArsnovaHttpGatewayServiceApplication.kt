45b3608ac5499498c3560d8d1be942cae1bfa2d3
Daniel Gerhardt <code@dgerhardt.net>
2022-06-07 12:30:54 +0200

    Add and initialize ReactorDebugAgent

--- gateway/src/main/kotlin/de/thm/arsnova/service/httpgateway/ArsnovaHttpGatewayServiceApplication.kt
+++ gateway/src/main/kotlin/de/thm/arsnova/service/httpgateway/ArsnovaHttpGatewayServiceApplication.kt
@@ -2,10 +2,12 @@ package de.thm.arsnova.service.httpgateway
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import reactor.tools.agent.ReactorDebugAgent
 
 @SpringBootApplication
 class ArsnovaHttpGatewayServiceApplication
 
 fun main(args: Array<String>) {
+    ReactorDebugAgent.init()
     SpringApplication.run(ArsnovaHttpGatewayServiceApplication::class.java, *args)
 }
