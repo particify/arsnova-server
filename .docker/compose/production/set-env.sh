ea702137e4ebcfa98f9eee109087168f2467571d
Daniel Gerhardt <code@dgerhardt.net>
2022-10-20 08:52:04 +0200

    Delete Compose v1 configs and move Dockerfiles

--- .docker/compose/production/set-env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-ENV=$1
-ENV_FILE=docker-compose.$ENV.yml
-if [ ! -f "$ENV_FILE" ]; then
-  echo No configuration for environment \"$ENV\" found.
-  exit 1
-fi
-ln -sf docker-compose.$ENV.yml docker-compose.override.yml
-ln -sf .$ENV.env .env
