2a13865a67c3403091234076ce2d30f26cdbd348
Daniel Gerhardt <code@dgerhardt.net>
2022-10-20 12:51:37 +0200

    Update tags, image and service names for Docker Compose

--- .docker/compose/development/project-mapping-vars.sh
+++ .docker/compose/development/project-mapping-vars.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 declare -A TAG_VARS PROJECT_DIRS
 
-TAG_VARS[arsnova-web]=WEB_TAG
-TAG_VARS[arsnova-http-gateway]=HTTPGW_TAG
-TAG_VARS[arsnova-ws-gateway]=WSGW_TAG
-TAG_VARS[arsnova-auth-service]=AUTH_TAG
-TAG_VARS[arsnova-backend-core]=CORE_TAG
-TAG_VARS[arsnova-comment-service]=COMMENT_TAG
-TAG_VARS[arsnova-formatting-service]=FORMATTING_TAG
+TAG_VARS[arsnova-webclient]=WEBCLIENT_TAG
+TAG_VARS[arsnova-server-gateway]=GATEWAY_TAG
+TAG_VARS[arsnova-server-websocket]=WEBSOCKET_TAG
+TAG_VARS[arsnova-server-authz]=AUTHZ_TAG
+TAG_VARS[arsnova-server-core]=CORE_TAG
+TAG_VARS[arsnova-server-comments]=COMMENTS_TAG
+TAG_VARS[arsnova-server-formatting]=FORMATTING_TAG
 
-PROJECT_DIRS[arsnova-web]=arsnova-webclient
-PROJECT_DIRS[arsnova-backend-core]=arsnova-backend
+PROJECT_DIRS[arsnova-webclient]=webclient
+PROJECT_DIRS[arsnova-server-gateway]=gateway
+PROJECT_DIRS[arsnova-server-websocket]=websocket
+PROJECT_DIRS[arsnova-server-authz]=authz
+PROJECT_DIRS[arsnova-server-core]=core
+PROJECT_DIRS[arsnova-server-comments]=comments
+PROJECT_DIRS[arsnova-server-formatting]=formatting
