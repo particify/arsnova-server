91f5c40ddb0a0ff48813a5f30330df5762f391c1
Daniel Gerhardt <code@dgerhardt.net>
2022-11-07 10:05:34 +0000

    Fix instantiation of VoteSum through HQL
    After migrating to version 6, Hibernate chose the wrong constructor for
    the instantiation of `VoteSum`. Since it was not used, the second
    constructor has been removed.
    
    Fixes: #174
    Regression-MR: !567

--- comments/src/main/java/net/particify/arsnova/comments/model/VoteSum.java
+++ comments/src/main/java/net/particify/arsnova/comments/model/VoteSum.java
@@ -4,11 +4,6 @@ public class VoteSum {
   private String commentId;
   private int sum;
 
-  public VoteSum(final String commentId, final int sum) {
-    this.commentId = commentId;
-    this.sum = sum;
-  }
-
   public VoteSum(final String commentId, final long sum) {
     this.commentId = commentId;
     this.sum = (int) sum;
