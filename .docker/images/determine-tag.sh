11f182c73f0ba88495a5722a836fafa7503defe7
Daniel Gerhardt <code@dgerhardt.net>
2022-11-23 15:26:51 +0100

    Improve determination of image tag names
    Tag names can now be correctly determined if the image uses a path. E.g.
    some.registry.example/namespace/image:tag.
    
    Furthermore, underscores in the generated name have been replaced with dashes to
    follow the convention of official images on Docker Hub.

--- .docker/images/determine-tag.sh
+++ .docker/images/determine-tag.sh
@@ -20,22 +20,22 @@ if [ ! -f "$DOCKERFILE_PATH" ]; then
 fi
 read -r DOCKER_FROM <"$DOCKERFILE_PATH"
 
-VERSION=$(echo "$DOCKER_FROM" | sed -E 's/FROM [a-z0-9/_-]+:([^@]+).*/\1/')
+VERSION=$(echo "$DOCKER_FROM" | sed -E 's/FROM ([a-z0-9./_-]+\/)?[a-z0-9_-]+:([^@]+).*/\2/')
 if [ "$IMAGE_NAME" = arsnova-proxy ]; then
   # The local version is more meaningful as the base image version.
   # => Always include the local version in the tag name.
   # => Use the local version it as prefix.
-  BASE_IMAGE_NAME=$(echo "$DOCKER_FROM" | sed -E 's/FROM ([a-z0-9/_-]+):[^@]+.*/\1/')
+  BASE_IMAGE_NAME=$(echo "$DOCKER_FROM" | sed -E 's/FROM ([a-z0-9./_-]+\/)?([a-z0-9_-]+):[^@]+.*/\2/')
   if [ -z "$IMAGE_AFFIX" ]; then
     IMAGE_AFFIX=next
   fi
-  IMAGE_TAG="${IMAGE_AFFIX}_${BASE_IMAGE_NAME}-${VERSION}"
+  IMAGE_TAG="${IMAGE_AFFIX}-${BASE_IMAGE_NAME}-${VERSION}"
 else
   # The base image version is more meaningful as the local version.
   # => Use the local version as suffx.
   IMAGE_TAG="$VERSION"
   if [ -n "$IMAGE_AFFIX" ]; then
-    IMAGE_TAG="${VERSION}_${IMAGE_AFFIX}"
+    IMAGE_TAG="${VERSION}-${IMAGE_AFFIX}"
   fi
 fi
 printf "$IMAGE_TAG"
