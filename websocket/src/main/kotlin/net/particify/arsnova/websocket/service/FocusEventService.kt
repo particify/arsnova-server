00170850c58d88e9d2a0df8bad3f97c3d06d1548
Lukas Mauß <lukas.mauss@gmx.de>
2023-06-21 15:36:18 +0200

    Add endpoints to distribute focus state events and get latest event

---
+++ websocket/src/main/kotlin/net/particify/arsnova/websocket/service/FocusEventService.kt
@@ -0,0 +1,34 @@
+package net.particify.arsnova.websocket.service
+
+import net.particify.arsnova.websocket.event.FocusEvent
+import net.particify.arsnova.websocket.exception.UnauthorizedException
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.stereotype.Service
+
+@Service
+class FocusEventService(
+  private val rabbitTemplate: RabbitTemplate
+) {
+  companion object {
+    const val OWNER_ROLE_PREFIX = "OWNER-"
+  }
+
+  private val latestEvents = mutableMapOf<String, FocusEvent>()
+
+  fun distribute(roles: List<String>, roomId: String, focusEvent: FocusEvent) {
+    if (roles.contains(OWNER_ROLE_PREFIX + roomId)) {
+      latestEvents[roomId] = focusEvent
+      rabbitTemplate.convertAndSend(
+        "amq.topic",
+        "$roomId.focus.state.stream",
+        focusEvent
+      )
+    } else {
+      throw UnauthorizedException()
+    }
+  }
+
+  fun getLatestEvent(roomId: String): FocusEvent? {
+    return latestEvents[roomId]
+  }
+}
