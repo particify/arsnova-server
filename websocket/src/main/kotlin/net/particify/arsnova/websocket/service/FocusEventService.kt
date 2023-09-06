f8ff6db0606868b3ec10f66b9140ed519d932ada
Daniel Gerhardt <code@dgerhardt.net>
2023-09-06 19:48:28 +0200

    Allow editor role to modify focus state
    Fixes: #213

--- websocket/src/main/kotlin/net/particify/arsnova/websocket/service/FocusEventService.kt
+++ websocket/src/main/kotlin/net/particify/arsnova/websocket/service/FocusEventService.kt
@@ -11,12 +11,13 @@ class FocusEventService(
 ) {
   companion object {
     const val OWNER_ROLE_PREFIX = "OWNER-"
+    const val EDITOR_ROLE_PREFIX = "EDITOR-"
   }
 
   private val latestEvents = mutableMapOf<String, FocusEvent>()
 
   fun distribute(roles: List<String>, roomId: String, focusEvent: FocusEvent) {
-    if (roles.contains(OWNER_ROLE_PREFIX + roomId)) {
+    if (roles.contains(OWNER_ROLE_PREFIX + roomId) || roles.contains(EDITOR_ROLE_PREFIX + roomId)) {
       latestEvents[roomId] = focusEvent
       rabbitTemplate.convertAndSend(
         "amq.topic",
