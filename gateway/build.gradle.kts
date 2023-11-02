1fd1c16526186f8bacce0683fa23dc206f77e6f6
Daniel Gerhardt <code@dgerhardt.net>
2023-11-02 11:35:21 +0100

    Directly use version catalog for spotbugs-annotations
    This fixes the Gradle `getDeps` task (used for `./pdk update`) which for an
    unknown reason could not resolve this dependency using the platform BOM.
    
    Closes: #221

--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -32,7 +32,7 @@ dependencies {
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("io.projectreactor:reactor-test")
-  compileOnly("com.github.spotbugs:spotbugs-annotations")
+  compileOnly(libs.spotbugs.annotations)
 }
 
 tasks.withType<KotlinCompile> {
