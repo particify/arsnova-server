13fed0a77549fc55abe3c083ba4b075cf93d414b
Daniel Gerhardt <code@dgerhardt.net>
2023-11-29 15:29:08 +0100

    Add secret support for JWT decoding
    JWT secrets (using HMAC) can now be used as alternative to JWKS.

--- authz/src/main/kotlin/net/particify/arsnova/authz/config/JwtConfig.kt
+++ authz/src/main/kotlin/net/particify/arsnova/authz/config/JwtConfig.kt
@@ -1,22 +1,31 @@
 package net.particify.arsnova.authz.config
 
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.oauth2.jwt.JwtDecoder
 import org.springframework.security.oauth2.jwt.JwtDecoders
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
+import javax.crypto.spec.SecretKeySpec
 
 @Configuration
 class JwtConfig {
-  val logger = LoggerFactory.getLogger(this::class.java)
+  companion object {
+    const val SECRET_ALGORITHM = "HmacSHA256"
+  }
+
+  val logger: Logger = LoggerFactory.getLogger(this::class.java)
 
   @Bean
   fun jwtDecoder(authServiceProperties: AuthServiceProperties): JwtDecoder? {
-    if (authServiceProperties.security.jwt.idpIssuer == null) {
-      logger.debug("IdP issuer is not set.")
-      return null
+    if (authServiceProperties.security.jwt.idpIssuer != null) {
+      logger.debug("Setting up JWT decoder using JWKS of IdP issuer.")
+      return JwtDecoders.fromIssuerLocation(authServiceProperties.security.jwt.idpIssuer)
     }
-    logger.debug("Setting up JWT decoder for IdP issuer.")
-    return JwtDecoders.fromIssuerLocation(authServiceProperties.security.jwt.idpIssuer)
+
+    logger.debug("Setting up JWT decoder using secret.")
+    val key = SecretKeySpec(authServiceProperties.security.jwt.secret.toByteArray(), SECRET_ALGORITHM)
+    return NimbusJwtDecoder.withSecretKey(key).build()
   }
 }
