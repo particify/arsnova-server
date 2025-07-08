3ba9792b8e8dca472de3c9c6582f59561914edca
Daniel Gerhardt <dg_git@particify.de>
2025-07-08 17:51:22 +0200

    Use constructor injection
    This fixes a "possible null pointer dereference of null" reported by
    Spotbugs. The change also makes the injection consistent with the rest
    of the project.

--- websocket/src/main/kotlin/net/particify/arsnova/websocket/handler/StompMetaHandler.kt
+++ websocket/src/main/kotlin/net/particify/arsnova/websocket/handler/StompMetaHandler.kt
@@ -1,19 +1,14 @@
 package net.particify.arsnova.websocket.handler
 
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.messaging.simp.user.SimpUserRegistry
 import org.springframework.stereotype.Component
 
 @Component
-class StompMetaHandler {
+class StompMetaHandler(
+  private val simpUserRegistry: SimpUserRegistry,
+) {
   private val logger = LoggerFactory.getLogger(StompMetaHandler::class.java)
-  private lateinit var simpUserRegistry: SimpUserRegistry
-
-  @Autowired
-  fun setSimpUserRegistry(simpUserRegistry: SimpUserRegistry) {
-    this.simpUserRegistry = simpUserRegistry
-  }
 
   fun handleUserSubscribed(roomId: String) {
     logger.error("/topic/$roomId.comment.stream")
