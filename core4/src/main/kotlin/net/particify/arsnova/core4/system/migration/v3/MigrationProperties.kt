6f03f3952b6ac2fa3140145d7a4cd4870fc1f77f
Daniel Gerhardt <dg_git@particify.de>
2025-07-15 17:30:28 +0200

    Create external logins during user migration
    Authentication provider names from v3 can be mapped to provider IDs in
    the configuration. In case of missing mappings, users of the affected
    authentication provider will be skipped.
    
    Example configuration:
    
        persistence:
          v3-migration:
            authentication-provider-mapping:
              SAML: fc62cd95-bea9-4aa3-987d-fa976ef300f3

--- core4/src/main/kotlin/net/particify/arsnova/core4/system/migration/v3/MigrationProperties.kt
+++ core4/src/main/kotlin/net/particify/arsnova/core4/system/migration/v3/MigrationProperties.kt
@@ -4,7 +4,13 @@
 package net.particify.arsnova.core4.system.migration.v3
 
 import java.net.URI
+import java.util.UUID
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "persistence.v3-migration")
-data class MigrationProperties(val enabled: Boolean, val couchdbUrl: URI, val roomAccessUrl: URI)
+data class MigrationProperties(
+    val enabled: Boolean,
+    val couchdbUrl: URI,
+    val roomAccessUrl: URI,
+    val authenticationProviderMapping: Map<String, UUID> = mapOf()
+)
