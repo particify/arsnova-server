f3f57a7abab1922b703f7da8415b1bc9228260d7
Daniel Gerhardt <dg_git@particify.de>
2025-08-27 15:30:48 +0200

    Fix room settings endpoint for participant
    Participants may not create room settings, so handle access denied error
    gracefully.
    
    Closes: #341

--- core/src/main/java/net/particify/arsnova/core/controller/RoomSettingsController.java
+++ core/src/main/java/net/particify/arsnova/core/controller/RoomSettingsController.java
@@ -1,6 +1,9 @@
 package net.particify.arsnova.core.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +15,7 @@ import net.particify.arsnova.core.service.RoomSettingsService;
 @EntityRequestMapping(RoomSettingsController.REQUEST_MAPPING)
 public class RoomSettingsController extends AbstractEntityController<RoomSettings> {
   protected static final String REQUEST_MAPPING = "/room/{roomId}/roomsettings";
+  private static final Logger logger = LoggerFactory.getLogger(RoomSettingsController.class);
   private RoomSettingsService roomSettingsService;
 
   protected RoomSettingsController(
@@ -28,6 +32,15 @@ public class RoomSettingsController extends AbstractEntityController<RoomSetting
   @GetMapping("/")
   public RoomSettings getByRoomId(@PathVariable final String roomId) {
     final RoomSettings roomSettings = roomSettingsService.getByRoomId(roomId);
-    return roomSettings != null ? roomSettings : roomSettingsService.create(new RoomSettings(roomId));
+    if (roomSettings != null) {
+      return roomSettings;
+    }
+    RoomSettings newRoomSettings = new RoomSettings(roomId);
+    try {
+      newRoomSettings = roomSettingsService.create(newRoomSettings);
+    } catch (final AccessDeniedException e) {
+      logger.debug("Cannot create room settings.", e);
+    }
+    return newRoomSettings;
   }
 }
