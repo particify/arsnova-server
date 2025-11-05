d7d44cb6712a51d0c05ef3a13e551eac778d4a9a
Daniel Gerhardt <dg_git@particify.de>
2025-11-05 14:21:03 +0100

    Add additional operations for room member management
    The client needs to retrieve a user's ID from a display ID to be able to
    grant a role. Furthermore, a mutation (NYI) for inviting user's without
    account has been added to the schema.

--- core4/src/main/kotlin/net/particify/arsnova/core4/user/internal/api/UserQueryController.kt
+++ core4/src/main/kotlin/net/particify/arsnova/core4/user/internal/api/UserQueryController.kt
@@ -3,7 +3,11 @@
  */
 package net.particify.arsnova.core4.user.internal.api
 
+import kotlin.jvm.optionals.getOrNull
+import net.particify.arsnova.core4.user.QUser
 import net.particify.arsnova.core4.user.User
+import net.particify.arsnova.core4.user.internal.UserRepository
+import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.graphql.data.method.annotation.SchemaMapping
 import org.springframework.security.core.annotation.AuthenticationPrincipal
@@ -11,10 +15,15 @@ import org.springframework.stereotype.Controller
 
 @Controller
 @SchemaMapping(typeName = "Query")
-class UserQueryController {
+class UserQueryController(private val userRepository: UserRepository) {
   @QueryMapping
   fun currentUser(@AuthenticationPrincipal user: User): User {
     return user
+  }
+
+  @QueryMapping
+  fun userByDisplayId(@Argument displayId: String): User? {
+    return userRepository.findBy(QUser.user.username.eq(displayId)) { it.first().getOrNull() }
   }
 
   @SchemaMapping(typeName = "User", field = "verified")
