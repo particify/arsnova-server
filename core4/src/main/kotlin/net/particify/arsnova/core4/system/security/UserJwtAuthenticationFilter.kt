32e331b8893d2cfebb28d3d74c070293d2953ffa
Daniel Gerhardt <dg_git@particify.de>
2025-11-24 13:23:39 +0100

    Use OncePerRequestFilter as base for UserJwtAuthenticationFilter
    With virtual threads disabled, the filter does not need to run a second
    time for GraphQL.

--- core4/src/main/kotlin/net/particify/arsnova/core4/system/security/UserJwtAuthenticationFilter.kt
+++ core4/src/main/kotlin/net/particify/arsnova/core4/system/security/UserJwtAuthenticationFilter.kt
@@ -4,9 +4,8 @@
 package net.particify.arsnova.core4.system.security
 
 import jakarta.servlet.FilterChain
-import jakarta.servlet.ServletRequest
-import jakarta.servlet.ServletResponse
 import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import java.util.regex.Matcher
 import java.util.regex.Pattern
 import org.springframework.http.HttpHeaders
@@ -14,21 +13,23 @@ import org.springframework.security.core.Authentication
 import org.springframework.security.core.AuthenticationException
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
-import org.springframework.web.filter.GenericFilterBean
+import org.springframework.web.filter.OncePerRequestFilter
 
 @Component
 class UserJwtAuthenticationFilter(
     private val userJwtAuthenticationProvider: UserJwtAuthenticationProvider
-) : GenericFilterBean() {
+) : OncePerRequestFilter() {
   companion object {
     private val BEARER_TOKEN_PATTERN = Pattern.compile("Bearer (.*)", Pattern.CASE_INSENSITIVE)
   }
 
-  override fun doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
-    val request = request as HttpServletRequest
-    val jwtHeader = request.getHeader(HttpHeaders.AUTHORIZATION)
-    if (jwtHeader == null || SecurityContextHolder.getContext().authentication != null)
-        return chain.doFilter(request, response)
+  override fun doFilterInternal(
+      request: HttpServletRequest,
+      response: HttpServletResponse,
+      chain: FilterChain
+  ) {
+    val jwtHeader =
+        request.getHeader(HttpHeaders.AUTHORIZATION) ?: return chain.doFilter(request, response)
 
     val tokenMatcher: Matcher = BEARER_TOKEN_PATTERN.matcher(jwtHeader)
     if (!tokenMatcher.matches()) {
@@ -47,5 +48,9 @@ class UserJwtAuthenticationFilter(
     }
 
     chain.doFilter(request, response)
+  }
+
+  override fun shouldNotFilter(request: HttpServletRequest): Boolean {
+    return SecurityContextHolder.getContext().authentication != null
   }
 }
