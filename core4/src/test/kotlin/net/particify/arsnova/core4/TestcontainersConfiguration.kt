5d3c19b53b47c56b509b4c2fd8acd448ca29892f
Daniel Gerhardt <dg_git@particify.de>
2025-11-24 17:26:24 +0100

    Fix deprecation in Testcontainer config

--- core4/src/test/kotlin/net/particify/arsnova/core4/TestcontainersConfiguration.kt
+++ core4/src/test/kotlin/net/particify/arsnova/core4/TestcontainersConfiguration.kt
@@ -6,14 +6,14 @@ package net.particify.arsnova.core4
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.springframework.context.annotation.Bean
-import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.postgresql.PostgreSQLContainer
 import org.testcontainers.utility.DockerImageName
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
   @Bean
   @ServiceConnection
-  fun postgresqlContainer(): PostgreSQLContainer<*> {
+  fun postgresqlContainer(): PostgreSQLContainer {
     return PostgreSQLContainer(DockerImageName.parse("postgres:18-alpine")).withUsername("arsnova")
   }
 }
