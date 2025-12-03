8c4475d3c94c2ff7f3165e5830b4e929d2f0a71f
Daniel Gerhardt <dg_git@particify.de>
2025-12-03 17:42:01 +0100

    Remove obsolete repository code
    This code was only used for early prototyping.

--- core4/src/main/kotlin/net/particify/arsnova/core4/room/internal/RoomRepository.kt
+++ core4/src/main/kotlin/net/particify/arsnova/core4/room/internal/RoomRepository.kt
@@ -6,9 +6,7 @@ package net.particify.arsnova.core4.room.internal
 import java.util.UUID
 import net.particify.arsnova.core4.room.Room
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.graphql.data.GraphQlRepository
 
-@GraphQlRepository
 interface RoomRepository : JpaRepository<Room, UUID> {
   fun findOneByShortId(shortId: Int): Room?
 
