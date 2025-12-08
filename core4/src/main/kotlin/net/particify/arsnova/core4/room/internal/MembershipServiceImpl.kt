c54c7ed265358d5a9957c7d6f810fd60e70a6675
Daniel Gerhardt <dg_git@particify.de>
2025-12-08 14:37:07 +0100

    Add stats for active member count to Room
    Also, unused stats field for RoomMembership GraphQL type has been
    removed.
    
    Closes: #376

--- core4/src/main/kotlin/net/particify/arsnova/core4/room/internal/MembershipServiceImpl.kt
+++ core4/src/main/kotlin/net/particify/arsnova/core4/room/internal/MembershipServiceImpl.kt
@@ -3,12 +3,16 @@
  */
 package net.particify.arsnova.core4.room.internal
 
+import java.time.Duration
+import java.time.Instant
 import java.util.UUID
 import net.particify.arsnova.core4.room.Membership
 import net.particify.arsnova.core4.room.MembershipService
 import org.springframework.data.domain.ScrollPosition
 import org.springframework.data.domain.Window
 import org.springframework.stereotype.Service
+
+private const val LAST_ACTIVITY_THRESHOLD_MINUTES = 5L
 
 @Service
 class MembershipServiceImpl(private val membershipRepository: MembershipRepository) :
@@ -19,6 +23,15 @@ class MembershipServiceImpl(private val membershipRepository: MembershipReposito
 
   override fun findByUserId(userId: UUID, scrollPosition: ScrollPosition): Window<Membership> {
     return membershipRepository.findByUserId(userId, scrollPosition)
+  }
+
+  fun countActiveMembersByRoomIds(roomIds: List<UUID>): Map<UUID, Int> {
+    val counts =
+        countByRoomIdsAndLastActivityAtAfter(
+            roomIds, Instant.now().minus(Duration.ofMinutes(LAST_ACTIVITY_THRESHOLD_MINUTES)))
+    return counts.associate {
+      Pair(it.get(0, UUID::class.java), it.get(1, Long::class.java).toInt())
+    }
   }
 
   @Deprecated(
