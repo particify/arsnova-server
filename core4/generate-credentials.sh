b51b81420f1c774b7b7969c40121f9a96bcd8b13
Daniel Gerhardt <dg_git@particify.de>
2026-01-15 16:50:36 +0100

    Use dev credentials for SAML from filesystem
    This is closer to the production environment behavior. Also, this avoids
    having the credentials build into the JAR file.

--- core4/generate-credentials.sh
+++ core4/generate-credentials.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-DEST=src/main/resources/credentials
+DEST=credentials
 KEY_FILE=$DEST/rp.key
 CSR_FILE=$DEST/rp.csr
 CRT_FILE=$DEST/rp.crt
@@ -9,5 +9,5 @@ mkdir -p "$DEST"
 openssl genrsa -out "$KEY_FILE" 4096
 openssl req -new -key "$KEY_FILE" -out "$CSR_FILE" -subj "/CN=example.com"
 openssl x509 -req -days 3650 -in "$CSR_FILE" -signkey "$KEY_FILE" -out "$CRT_FILE"
-export SAML_SIGNING_CERTIFICATE="$(openssl x509 -in src/main/resources/credentials/rp.crt -outform der | base64 -w 0)"
+export SAML_SIGNING_CERTIFICATE="$(openssl x509 -in "$DEST/rp.crt" -outform der | base64 -w 0)"
 cat "$KEYCLOAK_REALM_FILE.template" | envsubst '$SAML_SIGNING_CERTIFICATE' > "$KEYCLOAK_REALM_FILE"
