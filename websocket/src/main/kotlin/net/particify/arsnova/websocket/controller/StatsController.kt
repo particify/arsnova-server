66a98af2f1f83794f0fa78840a3626da6fae2c6b
Daniel Gerhardt <dg_git@particify.de>
2026-02-11 11:11:25 +0100

    Make WebSocket stats accessible w/o legacy gateway
    Connection stats are now accessible through the new
    `/api/management/websocket` endpoint (ADMIN role required).
    
    Closes: #411

--- websocket/src/main/kotlin/net/particify/arsnova/websocket/controller/StatsController.kt
+++ websocket/src/main/kotlin/net/particify/arsnova/websocket/controller/StatsController.kt
@@ -1,22 +1,33 @@
 package net.particify.arsnova.websocket.controller
 
 import net.particify.arsnova.websocket.event.RoomSubscriptionEventDispatcher
+import net.particify.arsnova.websocket.exception.ForbiddenException
 import net.particify.arsnova.websocket.model.Stats
-import org.slf4j.LoggerFactory
+import net.particify.arsnova.websocket.security.JwtTokenUtil
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 
 @RestController("StatsController")
 class StatsController(
   private val roomSubscriptionEventDispatcher: RoomSubscriptionEventDispatcher,
+  private val jwtTokenUtil: JwtTokenUtil,
 ) {
   companion object {
     const val STATS_MAPPING = "/stats"
   }
 
-  private val logger = LoggerFactory.getLogger(this::class.java)
-
   @GetMapping(STATS_MAPPING)
-  fun getStats(): Mono<Stats> = Mono.just(Stats(roomSubscriptionEventDispatcher.getWsSessionCount()))
+  fun getStats(
+    @RequestHeader(
+      name = "Authorization",
+    ) bearer: String,
+  ): Mono<Stats> {
+    val roles = jwtTokenUtil.getRolesFromToken(bearer.removePrefix("Bearer "))
+    if (!roles.contains("ADMIN")) {
+      throw ForbiddenException()
+    }
+    return Mono.just(Stats(roomSubscriptionEventDispatcher.getWsSessionCount()))
+  }
 }
