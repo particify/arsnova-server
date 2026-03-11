09d2f46cc27f7b5e74e2cb050d81952e4bd7709a
Daniel Gerhardt <dg_git@particify.de>
2026-03-11 17:55:10 +0100

    Split up migrate method for better maintainabilty

--- core4/src/main/kotlin/net/particify/arsnova/core4/system/migration/v3/CouchdbMigrator.kt
+++ core4/src/main/kotlin/net/particify/arsnova/core4/system/migration/v3/CouchdbMigrator.kt
@@ -45,28 +45,7 @@ class CouchdbMigrator(
     val migrationStart = Instant.now()
     entityManager.unwrap<Session>(Session::class.java).jdbcBatchSize = JDBC_BATCH_SIZE
     do {
-      logger.trace(
-          "Loading CouchDB data for migration (design: {}, startkey: {})...", design, startKey)
-      val queryStart = Instant.now()
-      val result =
-          try {
-            couchdbClient
-                .get()
-                .uri {
-                  it.path("/_design/$design/_view/by_id")
-                      .queryParam("reduce", false)
-                      .queryParam("include_docs", true)
-                      .queryParam("startkey", startKey)
-                      .queryParam("limit", COUCHDB_RESULT_LIMIT + 1)
-                      .build()
-                }
-                .retrieve()
-                .body(typeReference<CouchdbResponse<T>>())!!
-          } catch (e: RestClientException) {
-            logger.error("Failed to parse {} document (startkey: {})", design, startKey)
-            throw e
-          }
-      logger.debug("Query took {} ms.", queryStart.until(Instant.now(), ChronoUnit.MILLIS))
+      val result = queryData<T>(startKey)
       result.rows.take(COUCHDB_RESULT_LIMIT).chunked(JDBC_BATCH_SIZE).forEach { chunk ->
         chunk.forEach {
           logger.trace("Migrating {} {}...", design, it.doc.id)
@@ -94,5 +73,33 @@ class CouchdbMigrator(
         count,
         design,
         migrationStart.until(Instant.now(), ChronoUnit.SECONDS))
+  }
+
+  @PublishedApi
+  internal final inline fun <reified T : Entity> queryData(startKey: String): CouchdbResponse<T> {
+    val design = T::class.simpleName
+    logger.trace(
+        "Loading CouchDB data for migration (design: {}, startkey: {})...", design, startKey)
+    val queryStart = Instant.now()
+    val result =
+        try {
+          couchdbClient
+              .get()
+              .uri {
+                it.path("/_design/$design/_view/by_id")
+                    .queryParam("reduce", false)
+                    .queryParam("include_docs", true)
+                    .queryParam("startkey", startKey)
+                    .queryParam("limit", COUCHDB_RESULT_LIMIT + 1)
+                    .build()
+              }
+              .retrieve()
+              .body(typeReference<CouchdbResponse<T>>())!!
+        } catch (e: RestClientException) {
+          logger.error("Failed to parse {} document (startkey: {})", design, startKey)
+          throw e
+        }
+    logger.debug("Query took {} ms.", queryStart.until(Instant.now(), ChronoUnit.MILLIS))
+    return result
   }
 }
